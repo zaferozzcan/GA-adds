44cdb2049563f4e68d22e26d048509b4558d1195
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-10-05 19:10:24 -0400

    Give ring frodo

--- week3/Lord-of-the-Rings/scripts/fellowship.js
+++ week3/Lord-of-the-Rings/scripts/fellowship.js
@@ -98,10 +98,15 @@ const makeMiddleEarth = () => {
   // ============
   const keepItSecretKeepItSafe = () => {
 
+
     // 1. create an empty div with an id of 'the-ring'
+    var ringDiv = $("<div>")
+    ringDiv.attr("id", "the-ring")
 
     // 2. add the ring as a child of Frodo
     // hint: Frodo does not have an id, but there is a command to retrieve all elements with a certain class. This should give you an array for you to access . . .
+
+    $("li")[0].append(ringDiv)
 
     // when you think you have given Frodo the ring, check in your Elements tab to see that it works correctly
 
