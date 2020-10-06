44d7d300ae082f61ff082be5c3fddb6a30009748
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-10-06 09:38:32 -0400

    Grab elemenst for the events--open close

--- week3/Bridge-Game/app.js
+++ week3/Bridge-Game/app.js
@@ -1,6 +1,24 @@
 // console.log($);
 // console.log('modal practice app.js is linked to this index.html');
 
-$( () => {
+$(() => {
   //write your code here
+
+  const $openBtn = $('#openModal');
+
+
+
+  const $modal = $('#modal');
+
+
+
+
+  const $closeBtn = $('#close');
+
+
+
+
+
+
+
 });
