aacd8fac5b373a136c10d2458a1890598e29a98c
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-10-06 11:37:22 -0400

    add some functionality

--- week3/Card-Flipping/app.js
+++ week3/Card-Flipping/app.js
@@ -2,16 +2,22 @@ $(function () {
 
     var card = $(".card").on("click", (event) => {
         $(event.currentTarget).toggleClass("card-back")
-    })
+    });
 
 
+    // function to flip a card
 
-
-
-
-
-
-
+    const playHand = () => {
+        if (handOfCards.length === 2) {
+            checkHand()
+        } else {
+            handOfCards.push($(event.currentTarget).children().eq(1).text())
+            // if after pushing the length is NOW 2
+            if (handOfCards.length === 2) {
+                playHand()
+            }
+        }
+    }
 
 
 })
