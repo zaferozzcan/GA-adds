eba8c8ef0d35038a978bdc46dc7ed2e7bb5d9c74
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-10-06 16:44:36 -0400

    I can send items to the list

--- week3/To-Do-App/app.js
+++ week3/To-Do-App/app.js
@@ -1,16 +1,35 @@
 
-
+{/* <div class="to-do-item">
+    <p id="to-do-item-value">To Do Item Here</p>
+    <button>COMPLETED</button>
+</div> */}
 
 $(function () {
 
     const toDoItemList = []
 
-    // get the text item from the input and submit into the list array upon click button
-
+    // push item to the list and send it to the DOM
     $("#submit").on("click", function () {
         var textItem = $("#input-box").val()
         toDoItemList.push(textItem)
+
+        for (let index = 0; index < toDoItemList.length; index++) {
+            var div = $("<div>");
+            div.addClass("to-do-item")
+            var p = $("<p>");
+            p.text(toDoItemList[index])
+            var button = $("<button>")
+            button.append("COMPLETED")
+            div.append(p)
+            div.append(button)
+            $("#to-do-list").append(div)
+        }
+
     })
+
+
+    // event listener to completed button 
+
 
 
 
