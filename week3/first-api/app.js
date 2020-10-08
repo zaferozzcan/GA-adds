c64f22883858a51d5a40685c6113dde63bcbce99
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-10-08 11:21:12 -0400

    can make ajax request and get promise

--- week3/first-api/app.js
+++ week3/first-api/app.js
@@ -1,7 +1,23 @@
 const baseURL = `http://www.omdbapi.com/?`
-const apiKey = `apikey=dd68ea7f`
+const apiKey = `apikey=`
 const queryType = `t=`
 let titleQuery = 'eraserhead'
 let queryURL = baseURL + apiKey + '&' + queryType + titleQuery
 
 console.log(queryURL)
+
+const movieData = $.ajax({
+    url: queryURL
+})
+
+const getMovie = () => {
+    $.ajax({
+        url: queryURL
+    }).then((movieData) => {
+        console.log(movieData)
+    }, (error) => {
+        console.error(error)
+    })
+}
+
+getMovie()
