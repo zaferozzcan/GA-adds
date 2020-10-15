f98819a327d45baeddc580713049f5c26091d068
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-10-15 01:07:10 -0400

    can get concated string

--- week3/calculator/app.js
+++ week3/calculator/app.js
@@ -2,15 +2,20 @@
 
 $(() => {
 
+    let clickNumber = [];
     $(".button").on("click", (e) => {
-        let clickArray = [];
-        clickArray.push($(e.target).eq(0)[0].innerText);
+        let clickedOp = []
         var buttonType = $(e.target).eq(0)[0].classList[1]
-        console.log(clickArray)
+
 
         if (buttonType === "button-number") {
-            $(".number-box").eq(0)[0].childNodes[1].innerText = clickArray.join("");
+            clickNumber.push($(e.target).eq(0)[0].innerText);
+            $(".number-box").eq(0)[0].childNodes[1].innerText = clickNumber.join("");
+        } if (buttonType === "button-operator") {
+            clickOp.push($(e.target).eq(0)[0].innerText);
+            $(".number-box").eq(0)[0].childNodes[3].innerText = clickArray.join("")
         }
+
 
 
 
