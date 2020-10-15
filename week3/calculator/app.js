18385396e7a8ac8ddeef7725d45dfde13b44bdea
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-10-15 10:09:18 -0400

    second number goes to first number

--- week3/calculator/app.js
+++ week3/calculator/app.js
@@ -3,21 +3,31 @@
 $(() => {
 
     let clickNumber = [];
+    let secondNumber = []
     $(".button").on("click", (e) => {
         let clickedOp = []
+        let result;
         var buttonType = $(e.target).eq(0)[0].classList[1]
 
 
         if (buttonType === "button-number") {
-            clickNumber.push($(e.target).eq(0)[0].innerText);
             $(".number-box").eq(0)[0].childNodes[1].innerText = clickNumber.join("");
-        } if (buttonType === "button-operator") {
-            clickOp.push($(e.target).eq(0)[0].innerText);
-            $(".number-box").eq(0)[0].childNodes[3].innerText = clickArray.join("")
+        } if (clickNumber.length != 0 && buttonType === "button-operator") {
+            clickedOp.push($(e.target).eq(0)[0].innerText);
+            $(".number-box").eq(0)[0].childNodes[3].innerText = clickedOp.join("")
+        } if (clickedOp.length != 0 && clickNumber.length != 0 && buttonType === "button-number") {
+            secondNumber.push($(e.target).eq(0)[0].innerText)
+            console.log("second number", $(".number-box").eq(0)[0].childNodes[5]);
+            $(".number-box").eq(0)[0].childNodes[5].innerText = secondNumber.join("")
         }
 
+        if (clickNumber && clickedOp && secondNumber && buttonType === $(e.target).eq(0)[0].innerHTML) {
+            if (clickedOp === "+") {
+                var res = Number(clickNumber) + Number(secondNumber)
+                $(".number-box").eq(0)[0].childNodes[7].innerText = res
+            }
 
-
+        }
 
 
 
