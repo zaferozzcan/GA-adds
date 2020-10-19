f6b6f73fbe977856a3f991c2502f8484fdf5aba1
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-10-19 11:38:40 -0400

    i can send html files in string

--- week5/simsons/server.js
+++ week5/simsons/server.js
@@ -1,1 +1,16 @@
 const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send(`
+    <html>
+        <body>
+            <h1>Oh ha!</h1>
+        </body>
+    </html>
+`);
+})
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
