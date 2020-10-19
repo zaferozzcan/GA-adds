b07a2a280292047a267afde43a7e7bfe43f82df7
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-10-19 15:14:14 -0400

    add more operations

--- week5/calculator/server.js
+++ week5/calculator/server.js
@@ -1,7 +1,7 @@
 
 // Make a calculator that takes two params from the URI and sends the sum to the browser!
 
-
+require('dotenv').config()
 const express = require('express');
 const { get } = require("http")
 const app = express();
@@ -11,13 +11,21 @@ app.get("/calc/:num1/:num2", (req, res) => {
     console.log(req.query);
     if (req.query.op == "add") {
         res.send(`The total is ${Number(req.params.num1) + Number(req.params.num2)}`)
+    } else if (req.query.op == "mul") {
+        res.send(`The total is ${Number(req.params.num1) * Number(req.params.num2)}`)
+    } else if (req.query.op == "min") {
+        res.send(`The total is ${Number(req.params.num1) - Number(req.params.num2)}`)
+    } else if (req.query.op == "div") {
+        res.send(`The total is ${Number(req.params.num1) / Number(req.params.num2)}`)
     } else {
-        res.send("invalid")
+        res.send("invalid operation")
     }
 
 })
 
 
-app.listen(3000, () => {
+const port = process.env.PORT;
+app.listen(port, () => {
     console.log('app is running on port: ');
 });
+
