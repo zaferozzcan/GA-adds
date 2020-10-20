11014b92190f56adade5ae1cf6b63868ceb547d3
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-10-20 11:38:37 -0400

    can send  data from an  object

--- week5/day2/superhero/server.js
+++ week5/day2/superhero/server.js
@@ -3,13 +3,26 @@ const express = require("express");
 const { get } = require("http")
 const app = express();
 
-const superheros = ['Superman', 'Wonder Woman', 'Black Panther']
+const superheros = [
+    {
+        name: "Lex Luthor",
+        powers: ["super brain"]
+    },
+    {
+        name: "Ares",
+        powers: ["shape shifting", "teleporting"]
+    },
+    {
+        name: "Killmonger",
+        powers: ["ability to fit in Black Panther's suit", "internet agreement that he looks badass"]
+    }
+]
 
 app.get("/superheroes", (req, res) => {
     res.send(superheros)
 });
 app.get("/superheroes/:index", (req, res) => {
-    res.send(superheros[req.params.index])
+    res.send(superheros[req.params.index].powers)
 })
 
 const port = process.env.PORT;
