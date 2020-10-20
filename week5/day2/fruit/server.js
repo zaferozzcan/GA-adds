058e784fe84ffa7c19d88e3cedc2ff2e4daa0cc1
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-10-20 10:51:28 -0400

    review of routing

--- week5/day2/fruit/server.js
+++ week5/day2/fruit/server.js
@@ -2,9 +2,17 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+// data
+const fruits = ["apple", "banana", "pear"];
+
+//routes
+app.get("/fruits/", (req, res) => {
+    res.send(fruits)
+})
+
 
 
 const port = process.env.PORT;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-})
+});
