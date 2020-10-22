33eee93b1695246144cbd04348a396759799c35a
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-10-22 12:35:07 -0400

    add delete method and functionalit

--- week5/day4/uncharted_planet-CRUD app/server.js
+++ week5/day4/uncharted_planet-CRUD app/server.js
@@ -1,5 +1,6 @@
 const { json } = require("body-parser");
 const express = require("express");
+const methodOverride = require('method-override');
 PORT = 3000;
 const app = express();
 
@@ -12,16 +13,20 @@ scientist = []
 app.use(express.static(__dirname + "/public"))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'));
+
+
+
 
 app.get("/scientist", (req, res) => {
     res.render("index.ejs", {
         scientist: scientist
     })
-})
+});
 
 app.get("/scientist/new", (req, res) => {
     res.render("new.ejs")
-})
+});
 
 app.post("/scientist", (req, res) => {
     console.log(req.body);
@@ -29,7 +34,11 @@ app.post("/scientist", (req, res) => {
     res.redirect("/scientist")
 });
 
+app.delete("/scientist/:id", (req, res) => {
+    scientist.splice(req.params.id, 1)
+    res.redirect("/scientist")
+});
 
 app.listen(PORT, () => {
     console.log("Server is running on port:", PORT);
-})
+});
