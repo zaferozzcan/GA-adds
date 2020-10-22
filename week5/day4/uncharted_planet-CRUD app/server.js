433ae242d40cab2d42072002e392fe64787d022b
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-10-22 11:19:51 -0400

    add a view for creating new scientist

--- week5/day4/uncharted_planet-CRUD app/server.js
+++ week5/day4/uncharted_planet-CRUD app/server.js
@@ -2,8 +2,20 @@ const express = require("express");
 PORT = 3000;
 const app = express();
 
+
+
+// data
+scientist = []
+
+
+app.use(express.static(__dirname + "/public"))
+
 app.get("/index", (req, res) => {
     res.render("index.ejs")
+})
+
+app.get("/new", (req, res) => {
+    res.render("new.ejs")
 })
 
 
