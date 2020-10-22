6c6af4ebc74f52f6384e9058625aa4444ad10b43
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-10-22 11:49:21 -0400

    add styling on views

--- week5/day4/uncharted_planet-CRUD app/server.js
+++ week5/day4/uncharted_planet-CRUD app/server.js
@@ -1,3 +1,4 @@
+const { json } = require("body-parser");
 const express = require("express");
 PORT = 3000;
 const app = express();
@@ -9,14 +10,24 @@ scientist = []
 
 
 app.use(express.static(__dirname + "/public"))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/index", (req, res) => {
-    res.render("index.ejs")
+app.get("/scientist", (req, res) => {
+    res.render("index.ejs", {
+        scientist: scientist
+    })
 })
 
-app.get("/new", (req, res) => {
+app.get("/scientist/new", (req, res) => {
     res.render("new.ejs")
 })
+
+app.post("/scientist", (req, res) => {
+    console.log(req.body);
+    scientist.push(req.body)
+    res.redirect("/scientist")
+});
 
 
 app.listen(PORT, () => {
