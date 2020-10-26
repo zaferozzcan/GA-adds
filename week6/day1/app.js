b1034822f4e1de32998698b9bdb8491e9558bfd5
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-10-26 10:24:05 -0400

    add index page and render the list on ot

--- week6/day1/app.js
+++ week6/day1/app.js
@@ -2,10 +2,25 @@ const express = require("express")
 PORT = 3000;
 const app = express();
 
-app.get("/new", (req, res) => {
+let fruits = []
+
+// middleware
+app.use(express.urlencoded({ extended: true }))
+
+app.get("/fruits", (req, res) => {
+    res.render("index.ejs", {
+        fruits: fruits
+    })
+})
+
+app.get("/fruits/new", (req, res) => {
     res.render("new.ejs")
 })
 
+app.post("/fruits", (req, res) => {
+    fruits.push(req.body)
+    res.redirect("/fruits")
+})
 
 
 app.listen(PORT, () => {
