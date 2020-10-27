dd5a98a664796aba76d826461198180bbf3254c8
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-10-27 14:10:51 -0400

    find the greatest sum of cols row and diognal

--- code-challenges/loop2d.js
+++ code-challenges/loop2d.js
@@ -1,26 +1,48 @@
-
-const bigArray =
-    [[887, -541, -430, -590, 117, 172, -319, -18],
-    [-269, 964, 209, 840, -456, 156, 365, -568],
-    [289, -41, 488, 198, 240, 124, -427, 214],
-    [452, 894, 968, -149, 683, 977, 741, -805],
-    [181, -714, -950, 107, 800, 751, -143, 380],
-    [152, 493, 707, 914, 37, 356, -625, 608],
-    [-345, 906, 83, 676, 723, 381, 557, -183],
-    [199, -943, -710, 565, 193, 315, 281, 245]];
+// Make a function that returns the greatest sum of a row, column or diagonal from a two dimensional array
 
 
-function transpose(matrix) {
-    return matrix[0].map((col, i) => matrix.map(row => row[i]));
+let arr = [[10, 20, 30],
+[40, 50, 60],
+[70, -80, 90]];
+
+function largestSum(arr) {
+    let rows = [];
+    let columns = [];
+    let diagnol = [];
+    let sumDiagDown = 0;
+    let sumDiagUp = 0;
+    for (let i = 0; i < arr.length; i++) {
+        let sumRow = 0;
+        let sumCol = 0;
+        for (let j = 0; j < arr[i].length; j++) {
+            sumRow += arr[i][j];
+            sumCol += arr[j][i];
+        }
+        sumDiagDown += arr[i][i];
+        sumDiagUp += arr[i][arr.length - 1 - i];
+        rows.push(sumRow);
+        columns.push(sumCol);
+    }
+    diagnol.push(sumDiagDown);
+    diagnol.push(sumDiagUp);
+
+    console.log(rows, columns, diagnol);
+    //console.log(Math.max(Math.max(...rows), Math.max(...columns),Math.max(...diagnol)));
+    return Math.max(Math.max(...rows), Math.max(...columns), Math.max(...diagnol))
+
 }
 
-const transposed = transpose(bigArray)
+console.log(largestSum(arr));
 
-var max = 0
-bigArray.forEach(item => {
-    // console.log(item);
-    item.reduce((acc, i) => {
-        return max > (acc + i) ? max : max = acc + i
-    })
-})
-console.log(max);
+
+
+
+
+
+
+
+
+
+
+
+
