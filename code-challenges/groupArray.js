c7d85ce3517abf0f62781fd91c92998cab9a944a
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-10-27 14:31:28 -0400

    group elements off an array by given size

--- code-challenges/groupArray.js
+++ code-challenges/groupArray.js
@@ -1,5 +1,20 @@
 
 
-function groupArray(arr, size) {
-    return arr.spli
+
+const arr = ["a", "b", "c", "d", "e", "f"]
+
+function groupArr(arr, size) {
+    const grouped = []
+    while (arr.length > 0) {
+        grouped.push(arr.slice(0, size))
+        arr = arr.slice(size)
+    }
+    return grouped
 }
+
+
+console.log(groupArr(arr, 2))
+
+
+
+// reminder slice method returns an array
