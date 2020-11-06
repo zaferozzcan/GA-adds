30f1c6f451b402e0db52d0b51c0f2dce411c30ff
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-11-05 22:53:10 -0500

    syncVsAsync

--- code-challenges/sycncVsAsync.js
+++ code-challenges/sycncVsAsync.js
@@ -30,18 +30,37 @@ console.log("Finishes");
 
 
 
-console.log("Starts");
+// console.log("Starts");
 
-function logInUser(email, password) {
-    setTimeout(() => {
-        return { userEmail: email }
-    });
-}
+// function logInUser(email, password) {
+//     setTimeout(() => {
+//         return { userEmail: email }
+//     });
+// }
 
-let user = logInUser("z@gmail.com", "12345")
-console.log(user);
+// let user = logInUser("z@gmail.com", "12345")
+// console.log(user);
 
-console.log("Finishes");
+// console.log("Finishes");
 // if we run the code as it above, the terminal output is like, Starts -> undefined ->Finishes
 // the reason why is that the user variable is called  before its value is given because the value of it is returned in the set time out function. 
 
+//-------------------------------
+
+// we can solve this problem by using call back functions
+
+console.log("Starts");
+
+function logInUser(email, password, callback) {
+    setTimeout(() => {
+        callback({ userEmail: email })
+    });
+}
+
+let user = logInUser("z@gmail.com", "12345", (user) => {
+    console.log(user);
+})
+
+console.log("Finishes");
+
+// in sum-up, with the idea of the callback function, we can get get/call the data before the execution finishes. In the previous example, since we did not tell the call stack that we have call back wait before finish execution, it finished before the data is obtained from the source. 
