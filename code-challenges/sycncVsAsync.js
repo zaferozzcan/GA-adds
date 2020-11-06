370514a56a094818b178a82301fac19c94ee8396
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-11-05 22:45:55 -0500

    syncVsAsync

--- code-challenges/sycncVsAsync.js
+++ code-challenges/sycncVsAsync.js
@@ -12,6 +12,8 @@
 
 // in the terminal we see it print out this: Start -> In-Between -> Finish
 
+
+/*
 console.log("Starts");
 
 setTimeout(() => {
@@ -20,3 +22,26 @@ setTimeout(() => {
 }, 1000)
 // setTimeout is sent to web apis in the browser to be taken care of, when it is time it is gotten back to the call stack to execute. that is why the codes are not blocked(not to be waited untill the five seconds is up in the setTimeout)
 console.log("Finishes");
+
+*/
+
+
+// some problems arise from the synchronous behavior of JavaScript like the following.
+
+
+
+console.log("Starts");
+
+function logInUser(email, password) {
+    setTimeout(() => {
+        return { userEmail: email }
+    });
+}
+
+let user = logInUser("z@gmail.com", "12345")
+console.log(user);
+
+console.log("Finishes");
+// if we run the code as it above, the terminal output is like, Starts -> undefined ->Finishes
+// the reason why is that the user variable is called  before its value is given because the value of it is returned in the set time out function. 
+
