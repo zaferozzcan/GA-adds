f5821db401934528d22390a2ab333a3d74c08abe
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-11-05 22:21:33 -0500

    syncVsAsync

--- code-challenges/sycncVsAsync.js
+++ code-challenges/sycncVsAsync.js
@@ -1,19 +1,22 @@
 // javascript is a single-threaded language. it runs codes line by line, so it does synchronously.
 
-function printMe() {
-    console.log("In-Between");
-}
+// function printMe() {
+//     console.log("In-Between");
+// }
 
-console.log("Starts");
+// console.log("Starts");
 
-printMe()
+// printMe()
 
-console.log("Finishes");
+// console.log("Finishes");
 
 // in the terminal we see it print out this: Start -> In-Between -> Finish
 
 console.log("Starts");
 
-printMe()
+setTimeout(() => {
+    // do this after a given time
+    console.log("In-between")
+}, 1000)
 
 console.log("Finishes");
