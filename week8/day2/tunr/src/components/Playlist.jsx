babfe1ae34551be7bdf3846ae7b0ea3dc2f6d5c5
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-11-10 13:15:23 -0500

    split the playlist into a component

---
+++ week8/day2/tunr/src/components/Playlist.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import Song from "./Song.jsx";
+
+class PlayList extends Component {
+  render() {
+    return (
+      <main>
+        <div>
+          <h3>Playlist 1</h3>
+          <table>
+            <thead>
+              <tr>
+                <th>Song</th>
+                <th>Arist</th>
+                <th>Time</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.props.playlist.map((song, index) => {
+                return <Song song={song} key={index} />;
+              })}
+            </tbody>
+          </table>
+        </div>
+      </main>
+    );
+  }
+}
+
+export default PlayList;
