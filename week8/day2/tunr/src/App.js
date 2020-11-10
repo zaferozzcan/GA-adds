416d19ef135d426f6c3646b7c1d8d5de33252e94
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-11-10 11:11:59 -0500

    add table adn data

--- week8/day2/tunr/src/App.js
+++ week8/day2/tunr/src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <form>
+        <label htmlFor="title">
+          Song
+          <input type="text" id="title" />
+        </label>
+        <label htmlFor="artist">
+          Artist
+          <input type="text" id="artist" />
+        </label>
+        <label htmlFor="time">
+          Time
+          <input type="text" id="time" />
+        </label>
+        <label>
+          <input type="submit" />
+        </label>
+      </form>
+
+      <main>
+        <div>
+          <h3>Playlist 1</h3>
+          <table>
+            <thead>
+              <tr>
+                <th>Song</th>
+                <th>Arist</th>
+                <th>Time</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Song</td>
+                <td>Artist</td>
+                <td>Time</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </main>
     </div>
   );
 }
