12a28347aedb19e0e9fb7bcbdb1faab8d54cf427
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-11-13 17:57:26 -0500

    can send and get data from backend api

--- week8/day5/bookmark-app/backend/controllers/bookmark_controller.js
+++ week8/day5/bookmark-app/backend/controllers/bookmark_controller.js
@@ -4,6 +4,28 @@ const mongoose = require("mongoose");
 const Bookmark = require("../models/bookmark");
 
 // get route
-bookmarkRouter.get("/");
+bookmarkRouter.get("/", (req, res) => {
+  Bookmark.find({}, (err, data) => {
+    if (!err) {
+      res.status(200).json(data);
+    } else {
+      res.status(400).json({ error: err.message });
+    }
+  });
+});
+
+// post route
+bookmarkRouter.post("/", (req, res) => {
+  Bookmark.create(req.body, (err, data) => {
+    console.log(err);
+    console.log(data);
+    if (!err) {
+      console.log(data);
+      res.status(200).json(data);
+    } else {
+      res.status(400).json({ error: err.message });
+    }
+  });
+});
 
 module.exports = bookmarkRouter;
--- week8/day5/bookmark-app/backend/server.js
+++ week8/day5/bookmark-app/backend/server.js
@@ -1,7 +1,29 @@
 require("../node_modules/dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT;
+const mongoURI = process.env.mongoURI;
 const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// DATABASE
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.once("open", () => {
+  console.log("connected to mongoose...");
+});
+
+// database err connection check
+mongoose.connection.on("error", (err) =>
+  console.log(err.message + " is Mongod not running?")
+);
+mongoose.connection.on("disconnected", () => console.log("mongo disconnected"));
 
 //controllers
 const bookmarkController = require("./controllers/bookmark_controller");
