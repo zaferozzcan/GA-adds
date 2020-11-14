54c8c2f17ccf2b49182dd513ce73c2bbce36f01a
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-11-13 19:02:05 -0500

    backend complete cors error solved

--- week8/day5/bookmark-app/backend/server.js
+++ week8/day5/bookmark-app/backend/server.js
@@ -1,9 +1,25 @@
-require("../node_modules/dotenv").config();
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const PORT = process.env.PORT;
 const mongoURI = process.env.mongoURI;
 const app = express();
+
+// cors
+const whitelist = ["http://localhost:3000"];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 // middleware
 app.use(express.json());
--- week8/day5/bookmark-app/client-side/src/App.js
+++ week8/day5/bookmark-app/client-side/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+const fetchURL = "http://localhost:3003/bookmarks";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bookmarks: [],
+    };
+    this.loadBookmarks = this.loadBookmarks.bind(this);
+  }
+  componentDidMount() {
+    this.loadBookmarks();
+  }
+
+  loadBookmarks() {
+    fetch(fetchURL)
+      .then((data) => {
+        return data.json();
+      })
+      .then((parsedData) => {
+        this.setState({
+          bookmarks: parsedData,
+        });
+      });
+  }
+  render() {
+    console.log(this.state.bookmarks);
+    return <div></div>;
+  }
 }
-
-export default App;
