3e9c39af817e06fa61452c39dd6827072ba26d86
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-11-30 11:02:35 -0500

    add fizbuzz

--- week11/day1/intro.py
+++ week11/day1/intro.py
@@ -7,3 +7,36 @@
 #     print("here is the print!")
 
 # print_name()
+# def strInterpolation():
+#     print("this is my first string: {} and here is teh second one:{}".format("hey", "how"))
+
+# strInterpolation()
+
+# ----- control  flow --------
+# my_age = 31
+# old_trshold= 35
+# def am_i_old():
+#     if(my_age>old_trshold):
+#         print("old")
+#     else:
+#         print("not old")
+# am_i_old()
+
+
+# print(14.9 / 5)
+# print(14 // 5)
+
+
+# fizzbuzz
+
+# for num in range(1,21):
+#     if(num % 3 == 0 and num % 5 == 0):
+#         print(num,"fizbuzz")
+#     elif(num % 3 == 0):
+#         print(num, "fizz")
+#     elif(num % 5 == 0):
+#         print(num, "buzz")
+#     else:
+#         print(num)
+
+
