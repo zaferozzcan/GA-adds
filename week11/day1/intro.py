3435167f2fcfcba8da2e58178881caecbe82c974
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-11-30 10:28:46 -0500

    funcs in python

--- week11/day1/intro.py
+++ week11/day1/intro.py
@@ -1,4 +1,9 @@
-# in python for naming convention use snake_case:)
-my_name = "zafer"
+# 1) in python for naming convention use snake_case:)
+# my_name = "zafer"
+# print(my_name)
 
-print(my_name)
+#2) white space is important in python to and when defining scope
+# def print_name():
+#     print("here is the print!")
+
+# print_name()
