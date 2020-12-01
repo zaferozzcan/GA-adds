cbe46ef7166901a8cfbd62dd09bb5b062d469cab
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-12-01 15:18:12 -0500

    add class methods examples

--- week11/day2/python_classes/poker_game.py
+++ week11/day2/python_classes/poker_game.py
@@ -3,11 +3,14 @@
 # # so we dont need to call it in anywhere in the code
 
 # class User:
+#     active_users = 0
+#     total_users= 0
 #     def __init__(self, first, last, age): # first, last and age is called instance attributes
 #         self.first = first
-#         self.last = last
+#         self.last = last 
 #         self.age = age
-    
+#         User.total_users +=1
+        
 #     def full_name(self):   # this is a instance method
 #         print("Hello I am {}{}, and {} years old".format(self.first, self.last,self.age))
 
@@ -16,33 +19,65 @@
 
 #     def birthday(self):
 #         self.age +=1
-        
+
+#     def login(self):
+#         User.active_users +=1
+
+#     def logout(self):
+#         User.active_users -= 1
+    
+#     def show_active_users(self):
+#         print("total active users {}".format(User.active_users))
     
 # user1 = User("Joe", 'Smith', 67)
-# user1.full_name()
-# user1.birthday()
-# user1.full_name()
+# user2 = User("Monica", "James", 55)
+# user1.login()
+# user2.login()
+# user1.show_active_users()
 
 
-class BankAccount:
-    def __init__(self, owner, balance=0):
-        self.owner = owner
-        self.balance= balance
-        print("This is {}'s bank account".format(self.owner))
+
+# class BankAccount:
+#     def __init__(self, owner, balance=0):
+#         self.owner = owner
+#         self.balance= balance
+#         print("This is {}'s bank account".format(self.owner))
     
-    def deposit(self, amount):
-        print("Deposit amount: {}".format(amount))
-        self.balance += amount
-        print("Your balance is {}".format(self.balance))
-        return self.balance
-    def withdraw(self, amount):
-        print("Wuthdraw amount: {}".format(amount))
-        self.balance -= amount
-        print("Your balance is {}".format(self.balance))
-        return self.balance
+#     def deposit(self, amount):
+#         print("Deposit amount: {}".format(amount))
+#         self.balance += amount 
+#         print("Your balance is {}".format(self.balance))
+#         return self.balance
+#     def withdraw(self, amount):
+#         print("Wuthdraw amount: {}".format(amount))
+#         self.balance -= amount
+#         print("Your balance is {}".format(self.balance))
+#         return self.balance
         
+# bank_account1 = BankAccount("Zafer")
+# print(bank_account1)
+# bank_account1.deposit(1000)
+# bank_account1.withdraw(250)
 
-bank_account1 = BankAccount("Zafer")
-print(bank_account1)
-bank_account1.deposit(1000)
-bank_account1.withdraw(250)
+
+
+
+# class Pet:
+#     allowed = ["cat", "dog", "fist", "rat"] # this is a class attribute
+#     def __init__(self, name,species):
+#         if species not in Pet.allowed:
+#             raise ValueError("{} cannot be a pet".format(species))
+#         else:
+#             self.name= name
+#             self.species = species
+#     def set_species(self, newSpecies):
+#         if newSpecies not in Pet.allowed:
+#             raise ValueError("Your cannot change species to {}".format(newSpecies))
+#         else:
+#             self.species = newSpecies
+
+
+
+# cat = Pet("Blue", "cat")
+# dog = Pet("Wyatt", "dog")
+
