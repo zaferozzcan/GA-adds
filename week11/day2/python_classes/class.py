4d4b07d545614e6b5bc127f9a2811adaccf35a3d
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-12-01 11:20:28 -0500

    add inheritence examples

--- week11/day2/python_classes/class.py
+++ week11/day2/python_classes/class.py
@@ -1,13 +1,44 @@
-class Dog():
-    def __init__(self, name="", age="",color=""):
-        self.name=name
-        self.age=age
-        self.color=color
-        print(name, "created:", self)
-    
-    def bark_hello(self):
-        print("Woof! I am called", self.name, "and I am", self.age, "human-years old.")
+# class Dog():
+#   total_dogs = 0
+#   def __init__(self, name="", age=0 ,color=""):
+#     self.name = name
+#     self.age = age
+#     self.color = color
+#     Dog.total_dogs += 1
+#     print(name, "created:", self)
 
-kopuk = Dog("Kopuk", 8)
+#   def bark_hello(self):
+#       print("Woof! I am called", self.name, "I am", self.color, "and I am", self.age, "human-years old.")
+#       print("There are", Dog.total_dogs, "dogs in this room!")
 
-kopuk.bark_hello()
+
+# kopuk = Dog("Kopuk", 8,"white")
+# kopuk.bark_hello()
+
+
+
+class Parent():
+    def __init__(self):
+        self.first_name="Lorelei"
+        self.last_name="Gilmore"
+        print("Prent initialize:", self)
+
+    def hello(self):
+        print("Hey, I am ", self.first_name,". welcome to Dragonfly")
+
+
+class Child(Parent):
+    def __init__(self):
+        Parent.__init__(self)
+        self.first_name = "Rory"
+        print("Child Initialized:",self)
+
+
+
+mom = Parent()
+
+dougther = Child()
+
+mom.hello()
+
+dougther.hello()
