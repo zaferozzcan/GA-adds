33cf112a0e5a418474186ec42ed0bac2d7125bbd
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-12-01 10:48:50 -0500

    first python class example

--- week11/day2/python_classes/class.py
+++ week11/day2/python_classes/class.py
@@ -1,7 +1,9 @@
 class Dog():
-    def __init__(self, name="", age=""):
+    def __init__(self, name="", age="",color=""):
         self.name=name
         self.age=age
+        self.color=color
+        print(name, "created:", self)
     
     def bark_hello(self):
         print("Woof! I am called", self.name, "and I am", self.age, "human-years old.")
