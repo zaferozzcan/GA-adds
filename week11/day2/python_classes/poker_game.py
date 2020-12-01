04ebcd998bd4f0ca6eeb7e06685f0e9b3a7d33bc
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-12-01 15:38:25 -0500

    add class method more exampe

--- week11/day2/python_classes/poker_game.py
+++ week11/day2/python_classes/poker_game.py
@@ -2,38 +2,53 @@
 # # anytime we make an instance of a class, python runs _init method automatically
 # # so we dont need to call it in anywhere in the code
 
-# class User:
-#     active_users = 0
-#     total_users= 0
-#     def __init__(self, first, last, age): # first, last and age is called instance attributes
-#         self.first = first
-#         self.last = last 
-#         self.age = age
-#         User.total_users +=1
+class User:
+    active_users = 0
+    total_users= 0
+
+    @classmethod
+    def display_total_user(cls):
+        print("there are currently {} total user created!".format(User.total_users))
+
+    @classmethod
+    def instance_from_whole_string(cls, string):
+        first, last , age = string.split(",")
+        return cls(first, last, int(age))
+
+
+    def __init__(self, first, last, age): # first, last and age is called instance attributes
+        self.first = first
+        self.last = last 
+        self.age = age
+        User.total_users +=1
         
-#     def full_name(self):   # this is a instance method
-#         print("Hello I am {}{}, and {} years old".format(self.first, self.last,self.age))
+    def full_name(self):   # this is a instance method
+        print("Hello I am {}{}, and {} years old".format(self.first, self.last,self.age))
 
-#     def likes(self, thing):
-#         print("{} likes {}".format(self.first, thing))
+    def likes(self, thing):
+        print("{} likes {}".format(self.first, thing))
 
-#     def birthday(self):
-#         self.age +=1
+    def birthday(self):
+        self.age +=1
 
-#     def login(self):
-#         User.active_users +=1
+    def login(self):
+        User.active_users +=1
 
-#     def logout(self):
-#         User.active_users -= 1
+    def logout(self):
+        User.active_users -= 1
     
-#     def show_active_users(self):
-#         print("total active users {}".format(User.active_users))
+    def show_active_users(self):
+        print("total active users {}".format(User.active_users))
     
-# user1 = User("Joe", 'Smith', 67)
-# user2 = User("Monica", "James", 55)
-# user1.login()
-# user2.login()
-# user1.show_active_users()
+user1 = User("Joe", 'Smith', 67)
+user2 = User("Monica", "James", 55)
+user1.login()
+user2.login()
+
+# User.display_total_user()
+user3 = User.instance_from_whole_string("Zafer,Ozcan,58")
+
+user3.full_name()
 
 
 
