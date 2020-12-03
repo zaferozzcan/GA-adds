7fb9fc6f8e6a9af062099fbb68e5d7bfe07ae6ef
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-12-03 18:06:15 -0500

    can render template

--- Python/Flask_Crush/vars.py
+++ Python/Flask_Crush/vars.py
@@ -1,10 +1,10 @@
-from flask import Flask 
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hi():
-    return "Selam"
+    return render_template('index.html')
 
 @app.route('/<string:name>')
 def greeting(name):
