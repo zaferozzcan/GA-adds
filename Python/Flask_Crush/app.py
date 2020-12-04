7ec96378406d6bc6f77701562fbc88f047928b0e
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-12-03 19:17:09 -0500

    can get and post requests

--- Python/Flask_Crush/app.py
+++ Python/Flask_Crush/app.py
@@ -1,13 +1,16 @@
-from flask import Flask 
+from flask import Flask, render_template, request
 
 app = Flask(__name__) #/ creating app instance
 
 
-@app.route("/")
-def hel_world():
-    return '<h1>Hello World<h1>'
+# @app.route("/", methods=["GET"])
+# def about():
+#     return render_template("about.html")
 
-
+@app.route("/get", methods=["GET","POST"])
+def about_GET():
+    print(request.form)
+    return render_template("form.html")
 
 if __name__ =="__main__":
     app.run(debug=True)
