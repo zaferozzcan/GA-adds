12ba3bfe0094b9280950b71200030325ba056796
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-12-03 19:47:55 -0500

    add temps

--- Python/Flask_Crush/app.py
+++ Python/Flask_Crush/app.py
@@ -1,16 +1,23 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__) #/ creating app instance
 
 
-# @app.route("/", methods=["GET"])
-# def about():
-#     return render_template("about.html")
+@app.route("/", methods=["GET"])
+def about():
+    return render_template("about.html")
+
+@app.route("/<string:name>")
+def greeting(name):
+    return render_template("hello.html", name=name)
 
 @app.route("/get", methods=["GET","POST"])
 def about_GET():
-    print(request.form)
+    if(request.method=="POST"):
+        name = request.form["name"]
+        return redirect("/<string:name>")
     return render_template("form.html")
+
 
 if __name__ =="__main__":
     app.run(debug=True)
