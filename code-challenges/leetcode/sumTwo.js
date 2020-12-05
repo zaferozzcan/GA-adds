906f63f96a905d9e40e023be8eccc68950867721
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-12-05 15:24:59 -0500

    add sumtwo

--- code-challenges/leetcode/sumTwo.js
+++ code-challenges/leetcode/sumTwo.js
@@ -9,3 +9,18 @@ Example 1:
 Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
 Output: Because nums[0] + nums[1] == 9, we return [0, 1].  */
+function twoSum_On_Best(arr, target) {
+  let numObject = {};
+  for (var i = 0; i < arr.length; i++) {
+    let thisNum = arr[i];
+    numObject[thisNum] = i;
+  }
+  for (var i = 0; i < arr.length; i++) {
+    let diff = target - arr[i];
+    if (numObject.hasOwnProperty(diff) && numObject[diff] !== i) {
+      return [i, numObject[diff]];
+    }
+  }
+}
+console.log(twoSum_On_Best([2, 7, 11, 15], 9));
+// Output [ 0, 1 ]
