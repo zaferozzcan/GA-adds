1cb50fd7fb760dc899dcfccc93e9a62dae56b9d5
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-12-07 23:46:13 -0500

    push unknown updates

--- code-challenges/leetcode/sumTwo.js
+++ code-challenges/leetcode/sumTwo.js
@@ -1,4 +1,4 @@
-/* Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+/* Given an numsay of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
@@ -9,14 +9,14 @@ Example 1:
 Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
 Output: Because nums[0] + nums[1] == 9, we return [0, 1].  */
-function twoSum_On_Best(arr, target) {
+function twoSum_On_Best(nums, target) {
   let numObject = {};
-  for (var i = 0; i < arr.length; i++) {
-    let thisNum = arr[i];
+  for (var i = 0; i < nums.length; i++) {
+    let thisNum = nums[i];
     numObject[thisNum] = i;
   }
-  for (var i = 0; i < arr.length; i++) {
-    let diff = target - arr[i];
+  for (var i = 0; i < nums.length; i++) {
+    let diff = target - nums[i];
     if (numObject.hasOwnProperty(diff) && numObject[diff] !== i) {
       return [i, numObject[diff]];
     }
