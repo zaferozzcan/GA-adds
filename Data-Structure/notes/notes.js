2a9c3e566d8b07d1f4831a2240f9b8797e8e4ccf
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-12-19 10:23:54 -0500

    into Big O

---
+++ Data-Structure/notes/notes.js
@@ -0,0 +1,9 @@
+// Data structure allows up to talk about how the runtime of an algorithm grows  as the imput grows
+
+function addUp(n) {
+  var total = 0;
+  for (var i = 0; i < n; i++) {
+    total = +i;
+  }
+  return total;
+}
--- code-challenges/leetcode/longest_substring_w_no_reperat.js
+++ code-challenges/leetcode/longest_substring_w_no_reperat.js
@@ -6,18 +6,30 @@
 // concat every item until it repeats one of those before
 // try to catch seq
 
-function getLenSub(s) {
-  let seq = "";
-  let goneOver = [s[0]];
+// function getLenSub(s) {
+//   let maxSeq = "";
+//   let subSeq = "";
+//   let maxLen = 0;
+//   let mainIndex = 0;
+//   for (let i = 0; i < s.length; i++) {
+//     if (i == 0) {
+//       subSeq = subSeq + s[i];
+//       maxLen++;
+//     } else {
+//       if (s[i] != subSeq[mainIndex]) {
+//         console.log(subSeq);
+//         subSeq = subSeq + s[i];
+//         maxLen++;
+//       } else {
+//         mainIndex++;
+//         maxLen = subSeq.length;
+//         subSeq = "";
+//       }
+//     }
+//   }
+//   console.log(maxLen);
+// }
 
-  for (let i = 1; i < s.length; i++) {
-    if (i == 1) {
-      seq = seq + s[0];
-    }
-    if (!goneOver.includes(s[i])) {
-      seq = seq + s[i];
-      goneOver.push(s[i]);
-    }
-    console.log(seq);
-  }
-}
+// getLenSub("abcabk");
+
+console.log(!!!true);
