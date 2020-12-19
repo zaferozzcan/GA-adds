08decc609895fbe6025c56988cbfcd3cc3a66e26
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-12-19 11:14:59 -0500

    into Big O Space Complexity

--- Data-Structure/notes/notes.js
+++ Data-Structure/notes/notes.js
@@ -1,4 +1,4 @@
-// Data structure allows up to talk about how the runtime of an algorithm grows  as the imput grows
+// Data structure allows up to talk about how the runtime of an algorithm grows  as the imput grows (Big O's Time  complexity) -- i will touch on Space coplexity below!
 
 // we sat that an algorithm if O(f(n)) if the number of simple operations the computer has to di is eventually less than a constant times f(n) as n increases!
 // -- (fn) could be linear --(fn)=n, quadratic (fn)==n2, constant (fn)==1
@@ -36,3 +36,22 @@ function addUpUp() {
     }
   }
 }
+
+// When we are analyzing the Big O, we are not spesificatly looking at number of operations, --are anly looking at the general trend
+// O(2n) = trend is still O(n)
+// O(500) = trend is still O(1)
+//  O(13n^2) = trend is still O(n^2)
+// O(n^2 + 5n + 8 ) trend is still n^2
+
+////////////////////////////////////////////
+////////// Space Complexity/////////////////
+////////////////////////////////////////////
+
+// We can use Big O to measure space complexity of an algorithm as we did for the time comlexity
+// We will focus on the term-- "auxiliary space complexity" to refer to space required by the algorithm, ot including space taken up by the inputs.
+
+//////-------------------////////
+//Rule of thumps
+// Most primitives like booleans, numbers, undefined, null are constant space O(1) space
+// String require O(n) space(where n is the string length)
+// Reference types are generally O(n) space, where n is the length of for arrays, or the number of the keys of an objects
