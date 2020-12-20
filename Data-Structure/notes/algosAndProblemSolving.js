06ab99ca1051e92bbf347d22b165aa8c943e2070
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-12-20 13:40:51 -0500

    questions to be asked before starting to solve algo problems

---
+++ Data-Structure/notes/algosAndProblemSolving.js
@@ -0,0 +1,33 @@
+/*
+    WHATS IS AN ALGORITHMS
+    A process and set of steps to accomplish a certain task!
+    
+    HOW TO IMPROVE PROBLEM SOLVING SKILL
+    1) Devise a plan for solving problem
+    2) Master common problem solving problems
+
+    STEPS WHEN PROBLEM SOLVING
+    -- Understand the problem
+    -- Explore concrate examples
+    -- Break It Down
+    -- Solve/Simplify
+    -- Look back and refactor
+
+    QUESTIONS TO ANSWER BEFORE STARTING TO SOLVE PROBMEN
+
+    1) Can I restate the propblem in my own words?
+    2) What are the inputs that go into the problem?
+    3) What are the outputs that should come from the solutions to the problem?
+    4) Can the outputs be determined from the inputs? In other words, do I have enough information to solve the problem?
+    5) How should I lebel the importanat pieces of data that are a part of teh problem?
+
+    // apply these questions a problem "write a function that takes two number an returns sum of them!"
+        1) Can I restate the propblem in my own words?
+           "implement addition"
+        2) What are the inputs that go into the problem?
+            -- int? float? 
+        3) What are the outputs that should come from the solutions to the problem?
+            -- int? float? string?
+        4) Can the outputs be determined from the inputs? In other words, do I have enough information to solve the problem?
+        5) How should I lebel the importanat pieces of data that are a part of teh problem?
+ */
