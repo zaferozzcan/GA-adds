d6b618e5002bd4d321ebff0ae682a83a252d0e6b
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-12-20 12:13:34 -0500

    big O objects intro

---
+++ Data-Structure/notes/objects.js
@@ -0,0 +1,13 @@
+// An object is a collection of properties, and a property is an association between a name (or key) and a value
+
+// When to use objects
+//-When you do not need order
+//- When you need fast access/insertion and removal
+
+// big o of objects operations
+// insertion O(1)
+// Removal O(1)
+// Searchning O(1)
+// Access O(1)
+
+// Seems like you dont need any ordering, objects are an excellext choice.
