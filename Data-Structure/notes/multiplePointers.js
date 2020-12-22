7638c9af83682621c03c4213659c46ddb0b42d5f
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-12-22 18:34:37 -0500

    add multiple pointers another example

--- Data-Structure/notes/multiplePointers.js
+++ Data-Structure/notes/multiplePointers.js
@@ -26,3 +26,22 @@ function sumZero(arr) {
 }
 
 // console.log(sumZero([-1, 0, 1, 2, 3, 4]));
+
+/* Problem-2 Implement a function that accepts a sorted array, and count the unique value in the array, amek sure the array input has to be sorted! */
+
+function countUniqueValues(arr) {
+  if (arr.length === 0) return 0;
+
+  let i = 0;
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return i + 1;
+}
+
+// console.log(countUniqueValues([1, 1, 1, 2, 2, 2, 3, 4, 5, 5, 6, 6, 7, 8]));
+// console.log(countUniqueValues([1, 1, 1, 1, 1, 2]));
+console.log(countUniqueValues([]));
