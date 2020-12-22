d8c42238d9ca43009e5699111e73201a3fe8cd9b
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-12-21 23:30:19 -0500

    intro multiple pointers problem solving pattern

---
+++ Data-Structure/notes/multiplePointers.js
@@ -0,0 +1,9 @@
+/* 
+
+Creating pointers or values that correspons to an index  or position and move towards the beginning, end or middle based on a certain condition
+
+$$ Very efficient for solving problems with minimal space complexity as well $$
+
+Example Problem: 🤠 write a function that accepts a sorted array if integers. The function should find the first pair where the sum is 0. Return an array that includes both values that sum to zer or undefined if a pair does not exist
+See an solution below for multiple pointers
+*/
