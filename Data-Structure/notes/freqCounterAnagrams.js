c19d9bf7294da18ac98a5deaed9e73a04253c25f
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-12-21 22:29:42 -0500

    freq counter anagrams example

---
+++ Data-Structure/notes/freqCounterAnagrams.js
@@ -0,0 +1,30 @@
+/* 
+    Given two strings, write a function to determine if the second sring is an anagram of the first. An anagram is a word, phrase, or name formed by rearranging the letters of another, such as "cinema", formed from "icemen"
+*/
+
+function validAnagram(str1, str2) {
+  let freqCounter1 = {};
+  let freqCounter2 = {};
+  //   check if the length of string is the same
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  for (var char of str1) {
+    freqCounter1[char] = (freqCounter1[char] || 0) + 1;
+  }
+  for (var char of str2) {
+    freqCounter2[char] = (freqCounter2[char] || 0) + 1;
+  }
+  for (var key in freqCounter1) {
+    if (!(key in freqCounter2)) {
+      return false;
+    }
+    if (freqCounter1[key] !== freqCounter2[key]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(validAnagram("refaz", "zafer"));
