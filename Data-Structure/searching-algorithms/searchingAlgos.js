c032d2cbc00c4d5d9ace4dfc86e39b45798a927e
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-12-23 17:15:23 -0500

    linear search example

--- Data-Structure/searching-algorithms/searchingAlgos.js
+++ Data-Structure/searching-algorithms/searchingAlgos.js
@@ -8,4 +8,16 @@ Objectives
 
 /* 
     LINEAR SEARCH
+    is going thru all the element  in an array check and make comparasion what you are looking for!
+    -> some js methods --namely indexOf, includes, find, findIndex,  do the linear search behind the schene.
+
+    function linearSearch(arr, value) {
+  for (let key in arr) {
+    if (arr[key] === value) return key;
+  }
+  return -1;
+}
+
+console.log(linearSearch([2, 3, 5, 6, 8, 9], 7));
+    
 */
