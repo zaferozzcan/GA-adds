fbc1146c4ba10aa649c3546ec18d1c688dd1d97b
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-12-22 19:50:34 -0500

    example of sliding window pattern

---
+++ Data-Structure/notes/Sliding-Window/slidingWindow.js
@@ -0,0 +1,26 @@
+/*  This pattern involc=ves creating a window which can either be an array or number from one position to another
+    Depending on a certain condition, the window either increase or closes(and a new window is created)
+
+    Very useful for keeping track of a subset of data in an array/string etc
+*/
+
+/* Example to this pattern:Write a function that accepts an array of integers and number called and n. the function should calculate the max sum of n consecutive elements in the array.
+
+ */
+
+// function maxSubArraySum(arr, num) {
+//   let maxSum = 0;
+//   let tempSum = 0;
+//   if (arr.length < num) return null;
+//   for (let i = 0; i < num; i++) {
+//     maxSum += arr[i];
+//   }
+//   tempSum = maxSum;
+//   for (let i = num; i < arr.length; i++) {
+//     tempSum = tempSum - arr[i - num] + arr[i];
+//     maxSum = Math.max(maxSum, tempSum);
+//   }
+//   return maxSum;
+// }
+
+console.log(maxSubArraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3));
