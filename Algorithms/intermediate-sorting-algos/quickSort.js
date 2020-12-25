99604fa18c3678f3f97a573bdcdea924ffd39da9
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-12-25 17:29:29 -0500

    implementation of quick sort

--- Algorithms/intermediate-sorting-algos/quickSort.js
+++ Algorithms/intermediate-sorting-algos/quickSort.js
@@ -75,26 +75,40 @@ then swap the current element with the element at the pivot index
 // PIVOT HELPER IMPLEMENTATION//
 // first off all the return of pivot is index --the correct index of an element
 
-function pivot(arr, start = 0, end = arr.length + 1) {
-  function swap(arr, i, j) {
-    var temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
-  }
+function pivot(arr, start = 0, end = arr.length - 1) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
 
-  var pivot = arr[start];
-  var swapIndex = 0;
+  // We are assuming the pivot is always the first element
+  let pivot = arr[start];
+  let swapIdx = start;
 
-  for (var i = start + 1; i < arr.length; i++) {
+  for (let i = start + 1; i <= end; i++) {
     if (pivot > arr[i]) {
-      swapIndex++;
-      swap(arr, swapIndex, i);
+      swapIdx++;
+      swap(arr, swapIdx, i);
     }
   }
-  swap(arr, start, swapIndex);
-  console.log(swapIndex);
-  console.log(arr);
-  return swapIndex;
+
+  // Swap the pivot from the start the swapPoint
+  swap(arr, start, swapIdx);
+  return swapIdx;
 }
 
-pivot([4, 8, 2, 1, 5, 7, 6, 3]);
+// ===============================================================//
+// IMPLEMENTING SORTING PART ITSELF
+// ===============================================================//
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right); //3
+    //left
+    quickSort(arr, left, pivotIndex - 1);
+    //right
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
+
+console.log(quickSort([100, -3, 2, 4, 6, 9, 1, 2, 5, 3, 23]));
