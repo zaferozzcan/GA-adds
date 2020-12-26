92a23b1a2bd850e73ea3dc86104d7ccb44d33b71
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-12-26 14:51:28 -0500

    add pop method to the data structure

--- Data-Structure/singly-link-list.js
+++ Data-Structure/singly-link-list.js
@@ -35,20 +35,63 @@ class SinglyLinkList {
     this.length++;
     return this;
   }
+  pop() {
+    if (!this.head) return undefined;
+    var current = this.head;
+    var newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    return current;
+  }
 }
 
 var myList = new SinglyLinkList();
-console.log(myList);
-myList.push("Hello");
-console.log("after push", myList);
-myList.push("Zafer");
-console.log("after  2nd push", myList);
 
-// adding push method to singly data structure
+myList.push("Hello");
+myList.push("Zafer");
+myList.push("Ozcan");
+console.log("before", myList.tail.val);
+myList.pop();
+console.log("after", myList.tail.val);
+
+// myList.push("Ozcan");
+
 /*
+adding PUSH method to singly data structure
 This function should accept a value
 create new node using the value passed to the function 
 if there is no head in the property on the list, set the head and tail to be the newly created node
 otherwise set the newly created property on the tail to be the new node and set the tail property on the list to be newly created node
 increment length by one
+
+implementation
+push(val) {
+    var newNode = new Node(val);
+    if (!this.head) {
+      // IF THE HEAD IS EMPTY
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+*/
+
+/*
+POP METHOD of the linked list
+Removing a node from the end of the list
+-pseudo
+if there are no nodes in the list, return undefined
+loop through the list until you reach the tail
+set the next property of the second to last node to be null
+devrement the lenghth of the list by 1
+return the value of the node removed
 */
