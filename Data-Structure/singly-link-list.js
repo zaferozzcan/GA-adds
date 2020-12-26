29b08c6e2e76c4428e4db1dad69d0a18d2edb699
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-12-26 14:23:50 -0500

    implement push method the data structure

--- Data-Structure/singly-link-list.js
+++ Data-Structure/singly-link-list.js
@@ -9,7 +9,7 @@ to do list for making link list
 
 
 */
-class node {
+class Node {
   constructor(val) {
     this.val = val;
     this.next = null;
@@ -22,4 +22,33 @@ class SinglyLinkList {
     this.head = null;
     this.tail = null;
   }
+  push(val) {
+    var newNode = new Node(val);
+    if (!this.head) {
+      // IF THE HEAD IS EMPTY
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
+
+var myList = new SinglyLinkList();
+console.log(myList);
+myList.push("Hello");
+console.log("after push", myList);
+myList.push("Zafer");
+console.log("after  2nd push", myList);
+
+// adding push method to singly data structure
+/*
+This function should accept a value
+create new node using the value passed to the function 
+if there is no head in the property on the list, set the head and tail to be the newly created node
+otherwise set the newly created property on the tail to be the new node and set the tail property on the list to be newly created node
+increment length by one
+*/
