6e5d3993ee4e048a89f0ce2dfebdb1a46d961cc1
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-12-27 14:13:40 -0500

    update shift

--- Data-Structure/singly-link-list.js
+++ Data-Structure/singly-link-list.js
@@ -67,11 +67,12 @@ class SinglyLinkList {
     if (!this.head) {
       this.head = newNode;
       this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+      this.length++;
+      return this;
     }
-    newNode.next = this.head;
-    this.head = newNode;
-    this.length++;
-    return this;
   }
 }
 
