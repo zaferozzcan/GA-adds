dade0a720ce8841b879f89c784c4477a50029ff8
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-12-28 15:05:16 -0500

    add REMOVE Method

--- Data-Structure/singly-link-list.js
+++ Data-Structure/singly-link-list.js
@@ -1,12 +1,22 @@
 /*
 It is a data structure contains a head, tail and length property.
 
+Both Arrays and Linked List can be used to store linear data of similar types, but they both have some advantages and disadvantages over each other. 
+
+1. An array is the data structure that contains a collection of similar type data elements whereas the Linked list is considered as a non-primitive data structure contains a collection of unordered linked elements known as nodes. 
+2. In the array the elements belong to indexes, i.e., if you want to get into the fourth element you have to write the variable name with its index or location within the square bracket while in a linked list though, you have to start from the head and work your way through until you get to the fourth element. 
+3. Accessing an element in an array is fast, while Linked list takes linear time, so it is quite a bit slower. 
+4. Operations like insertion and deletion in arrays consume a lot of time. On the other hand, the performance of these operations in Linked lists are fast. 
+5. Arrays are of fixed size. In contrast, Linked lists are dynamic and flexible and can expand and contract its size. 
+6. In an array, memory is assigned during compile time while in a Linked list it is allocated during execution or runtime. 
+7. Elements are stored consecutively in arrays whereas it is stored randomly in Linked lists. 
+8. The requirement of memory is less due to actual data being stored within the index in the array. As against, there is a need for more memory in Linked Lists due to storage of additional next and previous referencing elements. 
+9. In addition memory utilization is inefficient in the array. Conversely, memory utilization is efficient in the linked list. 
+
+
 Link lists consost of nodes, and each node has a value a nd pointer to another node or null.
-
-
 to do list for making link list
     -
-
 
 */
 class Node {
@@ -105,6 +115,16 @@ class SinglyLinkList {
     this.length++;
     return true;
   }
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+    var previousNode = this.get(index - 1);
+    var removed = previousNode.next;
+    previousNode.next = removed.next;
+    this.length--;
+    return removed;
+  }
 }
 
 var myList = new SinglyLinkList();
@@ -118,7 +138,6 @@ console.log(myList);
 /*
 
             IMPLEMENTATION
-
 
 */
 /*
@@ -227,3 +246,17 @@ increment the length
 return true
 
  */
+/*
+ REMOVE Method Pseudo 
+ if the index is less than zero or greater than the length return undefined 
+ if the index is the same as the length - 1, use POP method
+ if the index is 0, shift
+ otherwise, using the GET Method, access the node at the index - 1
+ set the next property on that node to be the next of the next node
+ decrement the length
+ return the value of the node removed
+  */
+
+var myArray = [1, 2, 3, 4];
+myArray.push(5);
+console.log(myArray);
