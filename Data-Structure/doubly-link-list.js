bedbb7286dde0d38f6f9e00194d1b81b2a3b88de
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-01-02 14:10:42 -0500

    add Doubly Link Lists

---
+++ Data-Structure/doubly-link-list.js
@@ -0,0 +1,16 @@
+//setting up the classes
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+}
