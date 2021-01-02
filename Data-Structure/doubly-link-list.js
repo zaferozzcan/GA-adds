dd7db588b701ff88453e29b0e8061d38eab701e5
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-01-02 14:18:36 -0500

    Doubly Link List push method

--- Data-Structure/doubly-link-list.js
+++ Data-Structure/doubly-link-list.js
@@ -12,5 +12,20 @@ class DoublyLinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    var newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
   }
 }
