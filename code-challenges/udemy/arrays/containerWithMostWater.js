ff8d75faef155482c8b6318472e9e58044de3910
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-01-09 12:39:23 -0500

    optimal solution to the most water question

--- code-challenges/udemy/arrays/containerWithMostWater.js
+++ code-challenges/udemy/arrays/containerWithMostWater.js
@@ -13,6 +13,28 @@ function mostWater(arr) {
   }
   return area;
 }
-console.log(mostWater([1, 7, 4, 2]));
+// console.log(mostWater([1, 7, 4, 2]));
 
 // OPTIMAL SOLUTION \\
+/* To improve the performance of the solution above, we can use a new technique like sliding window */
+
+const getMaxWaterContainer = function (heights) {
+  let p1 = 0,
+    p2 = heights.length - 1,
+    maxArea = 0;
+
+  while (p1 < p2) {
+    const height = Math.min(heights[p1], heights[p2]);
+    const width = p2 - p1;
+    const area = height * width;
+    maxArea = Math.max(maxArea, area);
+    if (heights[p1] <= heights[p2]) {
+      p1++;
+    } else {
+      p2--;
+    }
+  }
+  return maxArea;
+};
+
+console.log(getMaxWaterContainer([4, 8, 1, 2, 3, 9]));
