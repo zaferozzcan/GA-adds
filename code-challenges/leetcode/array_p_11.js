bf4eff83b681ba2ccf26b61684f4e35a2930ca9c
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-01-09 15:00:29 -0500

    add array problem 11 solution to leetcode

---
+++ code-challenges/leetcode/array_p_11.js
@@ -0,0 +1,20 @@
+// Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of the line i is at (i, ai) and (i, 0). Find two lines, which, together with the x-axis forms a container, such that the container contains the most water.
+
+const getMaxWaterContainer = function (heights) {
+  let p1 = 0,
+    p2 = heights.length - 1,
+    maxArea = 0;
+
+  while (p1 < p2) {
+    const height = Math.min(heights[p1], heights[p2]);
+    const width = p2 - p1;
+    const area = height * width;
+    maxArea = Math.max(maxArea, area);
+    if (heights[p1] <= heights[p2]) {
+      p1++;
+    } else {
+      p2--;
+    }
+  }
+  return maxArea;
+};
