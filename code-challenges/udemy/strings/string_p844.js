e6635ddac7e657e055694685d3c99e7b241ba384
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-01-10 14:01:00 -0500

    add optimal solution to the string 844 leetcode

--- code-challenges/udemy/strings/string_p844.js
+++ code-challenges/udemy/strings/string_p844.js
@@ -34,9 +34,6 @@ console.log(checkSame("zA#f", "Za#f"));
 
 // sticking to DRY Principle more
 
-const string1 = "ab#z";
-const string2 = "az#z";
-
 const buildString = function (string) {
   const builtString = [];
   for (let p = 0; p < string.length; p++) {
@@ -68,3 +65,54 @@ var backspaceCompare = function (S, T) {
 };
 
 console.log(backspaceCompare(string1, string2));
+
+// OPTIMIZE SOLUTION
+
+const string1 = "ab#z";
+const string2 = "az#z";
+
+var backspaceCompareOptim = function (S, T) {
+  let p1 = S.length - 1,
+    p2 = T.length - 1;
+
+  while (p1 >= 0 || p2 >= 0) {
+    if (S[p1] === "#" || T[p2] === "#") {
+      if (S[p1] === "#") {
+        let backCount = 2;
+
+        while (backCount > 0) {
+          p1--;
+          backCount--;
+
+          if (S[p1] === "#") {
+            backCount += 2;
+          }
+        }
+      }
+
+      if (T[p2] === "#") {
+        let backCount = 2;
+
+        while (backCount > 0) {
+          p2--;
+          backCount--;
+
+          if (T[p2] === "#") {
+            backCount += 2;
+          }
+        }
+      }
+    } else {
+      if (S[p1] !== T[p2]) {
+        return false;
+      } else {
+        p1--;
+        p2--;
+      }
+    }
+  }
+
+  return true;
+};
+
+console.log(backspaceCompareOptim(string1, string2));
