c649e0e4ee802ed51f6475f64b298c1b0b21da86
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-01-10 13:27:42 -0500

    add more dry version

--- code-challenges/udemy/strings/string_p.js
+++ code-challenges/udemy/strings/string_p.js
@@ -18,6 +18,8 @@ function checkSame(s, t) {
       cleanT += t[i];
     }
   }
+  cleanT = cleanT.toLowerCase();
+  cleanS = cleanS.toLowerCase();
   console.log(cleanS, cleanT);
   if (cleanS.length !== cleanT.length) return false;
   for (let j = 0; j < cleanT.length; j++) {
@@ -28,4 +30,41 @@ function checkSame(s, t) {
   return true;
 }
 
-console.log(checkSame("z##b", "za#f"));
+console.log(checkSame("zA#f", "Za#f"));
+
+// sticking to DRY Principle more
+
+const string1 = "ab#z";
+const string2 = "az#z";
+
+const buildString = function (string) {
+  const builtString = [];
+  for (let p = 0; p < string.length; p++) {
+    if (string[p] !== "#") {
+      builtString.push(string[p]);
+    } else {
+      builtString.pop();
+    }
+  }
+
+  return builtString;
+};
+
+var backspaceCompare = function (S, T) {
+  const finalS = buildString(S);
+  const finalT = buildString(T);
+
+  if (finalS.length !== finalT.length) {
+    return false;
+  } else {
+    for (let p = 0; p < finalS.length; p++) {
+      if (finalS[p] !== finalT[p]) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+};
+
+console.log(backspaceCompare(string1, string2));
