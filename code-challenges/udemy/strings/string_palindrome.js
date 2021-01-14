b0c13000b7b7600b241bb558a7509c9df15ffbd9
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-01-13 21:22:58 -0500

    valid palindrome

--- code-challenges/udemy/strings/string_palindrome.js
+++ code-challenges/udemy/strings/string_palindrome.js
@@ -6,3 +6,33 @@ let myString = "aBc--dfg";
 myString = myString.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
 
 console.log(myString);
+//
+var validPalindrome = function (s) {
+  s = s.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
+  let start = 0;
+  let end = s.length - 1;
+  while (start < end) {
+    if (s[start] !== s[end]) {
+      return (
+        validSubPalindrome(s, start + 1, end) ||
+        validSubPalindrome(s, start, end - 1)
+      );
+    }
+    start++;
+    end--;
+  }
+  return true;
+};
+
+var validSubPalindrome = function (s, start, end) {
+  while (start < end) {
+    if (s[start] !== s[end]) {
+      return false;
+    }
+    start++;
+    end--;
+  }
+  return true;
+};
+
+console.log(validPalindrome("rAce car"));
