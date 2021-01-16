412a73bcbbab53e272b3d12872579008c80bb7c6
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-01-15 20:25:33 -0500

    100daysOfPython day1

---
+++ 100daysOfPython/day1/warmup.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator!")
+city = input("What is the city you grew up in? \n")
+pet_name = input("What is your pet's name?\n")
+print("Your Band Name is "+ city + " " + pet_name)
