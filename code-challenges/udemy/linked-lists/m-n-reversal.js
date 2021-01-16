119ec8d14d202865582f7641577b08c99bfdb4a9
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-01-15 23:43:50 -0500

    add pop

--- code-challenges/udemy/linked-lists/m-n-reversal.js
+++ code-challenges/udemy/linked-lists/m-n-reversal.js
@@ -34,6 +34,23 @@ class singlyLinkedList {
     }
     console.log(arr);
   }
+  pop() {
+    if (!this.head) return undefined;
+    var current = this.head;
+    var newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
 }
 
 const myList = new singlyLinkedList();
