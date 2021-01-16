fe1c6730c901456ae3c057cc9231c280c40015e0
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-01-15 23:45:01 -0500

    go over shift

--- code-challenges/udemy/linked-lists/m-n-reversal.js
+++ code-challenges/udemy/linked-lists/m-n-reversal.js
@@ -51,6 +51,16 @@ class singlyLinkedList {
     }
     return current;
   }
+  shift() {
+    if (!this.head) return undefined;
+    var currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail === null;
+    }
+    return currentHead;
+  }
 }
 
 const myList = new singlyLinkedList();
