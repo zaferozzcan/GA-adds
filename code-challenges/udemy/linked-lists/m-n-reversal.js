690819cd08a148f8d2ff4f7fdcaf152aca1b8404
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-01-16 13:47:14 -0500

    add doubly linked list

--- code-challenges/udemy/linked-lists/m-n-reversal.js
+++ code-challenges/udemy/linked-lists/m-n-reversal.js
@@ -22,20 +22,25 @@ class singlyLinkedList {
   }
 
   push(val) {
-    var newNode = new Node(val);
+    let newNode = new Node(val);
     if (!this.head) {
-      // IF THE HEAD IS EMPTY
       this.head = newNode;
       this.tail = this.head;
     } else {
       this.tail.next = newNode;
       this.tail = newNode;
     }
-    this.length++;
-    return this;
   }
   pop() {
-    if (this.length < 1) return undefined;
+    if (!this.head) return undefined;
+    current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
   }
 }
 
