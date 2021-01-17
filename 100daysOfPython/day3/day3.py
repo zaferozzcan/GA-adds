11996c80a99838a93afa172aad84fcf8d6e0a6b7
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-01-17 12:14:58 -0500

    kondisyon exersizleri

---
+++ 100daysOfPython/day3/day3.py
@@ -0,0 +1,7 @@
+print("welcome to the rollercoster?")
+heigth = input("Enter your heigth? \n")
+
+if(int(heigth)>120):
+    print("You can ride.")
+else:
+    print("Sorry, you are too short to ride!")
