84623500911e9834770837984ab8c5518f915b02
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-01-17 14:34:09 -0500

    typescript tut financial tracker initial file

--- 100daysOfPython/day3/day3.py
+++ 100daysOfPython/day3/day3.py
@@ -41,34 +41,34 @@
 # peperoni for small +2
 # peperoni for medium or large+3
 
-print("Welcome! Order your pizza!")
+# print("Welcome! Order your pizza!")
 
-size = input("what size of pizza you like S,M,L\n")
+# size = input("what size of pizza you like S,M,L\n")
 
-if size == "S":
-    want_roni=input("Peperoni Y / N ?")
-    if want_roni == "Y":
-        print ("Your total is $17")
-    elif(want_roni == "N"):
-         print("Your total is $15")
-    else:
-         print("enter a valid answer")
-elif size == "M":
-     want_roni=input("Peperoni Y / N ?")
-     if want_roni == "Y":
-        print ("Your total is $22")
-     elif(want_roni == "N"):
-         print("Your total is $20")
-     else:
-         print("enter a valid answer")
-elif size == "L":
-     want_roni=input("Peperoni Y / N ?")
-     if want_roni == "Y":
-        print ("Your total is $27")
-     elif(want_roni == "N"):
-         print("Your total is $25")
-     else:
-         print("enter a valid answer")
+# if size == "S":
+#     want_roni=input("Peperoni Y / N ?")
+#     if want_roni == "Y":
+#         print ("Your total is $17")
+#     elif(want_roni == "N"):
+#          print("Your total is $15")
+#     else:
+#          print("enter a valid answer")
+# elif size == "M":
+#      want_roni=input("Peperoni Y / N ?")
+#      if want_roni == "Y":
+#         print ("Your total is $22")
+#      elif(want_roni == "N"):
+#          print("Your total is $20")
+#      else:
+#          print("enter a valid answer")
+# elif size == "L":
+#      want_roni=input("Peperoni Y / N ?")
+#      if want_roni == "Y":
+#         print ("Your total is $27")
+#      elif(want_roni == "N"):
+#          print("Your total is $25")
+#      else:
+#          print("enter a valid answer")
 
-else:
-    print("enter a valid size!")
+# else:
+#     print("enter a valid size!")
--- code-challenges/udemy/linked-lists/m-n-reversal.js
+++ code-challenges/udemy/linked-lists/m-n-reversal.js
@@ -1,34 +1,79 @@
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
+
+// class singlyLinkedList {
+//   constructor() {
+//     this.length = 0;
+//     this.head = null;
+//     this.tail = null;
+//   }
+//   print() {
+//     var arr = [];
+//     var current = this.head;
+//     while (current) {
+//       arr.push(current.val);
+//       current = current.next;
+//     }
+//     console.log(arr);
+//   }
+
+//   push(val) {
+//     let newNode = new Node(val);
+//     if (!this.head) {
+//       this.head = newNode;
+//       this.tail = this.head;
+//     } else {
+//       this.tail.next = newNode;
+//       this.tail = newNode;
+//     }
+//   }
+//   pop() {
+//     if (!this.head) return undefined;
+//     current = this.head;
+//     let newTail = current;
+//     while (current.next) {
+//       newTail = current;
+//       current = current.next;
+//     }
+//     this.tail = newTail;
+//     this.tail.next = null;
+//   }
+// }
+
+// const myList = new singlyLinkedList();
+
+// myList.push(1);
+// myList.push(2);
+// myList.push(3);
+// console.log(myList);
+
 class Node {
   constructor(val) {
     this.val = val;
     this.next = null;
   }
 }
-
-class singlyLinkedList {
+class LinkedList {
   constructor() {
-    this.length = 0;
     this.head = null;
     this.tail = null;
-  }
-  print() {
-    var arr = [];
-    var current = this.head;
-    while (current) {
-      arr.push(current.val);
-      current = current.next;
-    }
-    console.log(arr);
+    this.length = 0;
   }
 
   push(val) {
-    let newNode = new Node(val);
+    const newNode = new Node(val);
     if (!this.head) {
       this.head = newNode;
-      this.tail = this.head;
+      this.tail = newNode;
+      this.length++;
     } else {
       this.tail.next = newNode;
       this.tail = newNode;
+      this.length++;
     }
   }
   pop() {
@@ -44,9 +89,8 @@ class singlyLinkedList {
   }
 }
 
-const myList = new singlyLinkedList();
+let myList = new LinkedList();
 
 myList.push(1);
 myList.push(2);
-myList.push(3);
-console.log(myList);
+console.log(myList.pop());
