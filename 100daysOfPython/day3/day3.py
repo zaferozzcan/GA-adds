f0944e2dee8489a280d4de1b6de4d7805c9530e1
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-01-17 12:57:11 -0500

    add pizza order line

--- 100daysOfPython/day3/day3.py
+++ 100daysOfPython/day3/day3.py
@@ -22,15 +22,53 @@
 #     print('odd')
 
 
-year = input("enter a year\n")
+# year = input("enter a year\n")
 
-if(int(year)%4==0):
-    if(int(year)%100==0):
-        if(int(year)%400==0):
-            print("leap")
-        else:
-            print("not")
+# if(int(year)%4==0):
+#     if(int(year)%100==0):
+#         if(int(year)%400==0):
+#             print("leap")
+#         else:
+#             print("not")
+#     else:
+#         print("leap")
+# else:
+#     print("not")
+
+
+
+# small 15 md 20 large 25 
+# peperoni for small +2
+# peperoni for medium or large+3
+
+print("Welcome! Order your pizza!")
+
+size = input("what size of pizza you like S,M,L\n")
+
+if size == "S":
+    want_roni=input("Peperoni Y / N ?")
+    if want_roni == "Y":
+        print ("Your total is $17")
+    elif(want_roni == "N"):
+         print("Your total is $15")
     else:
-        print("leap")
+         print("enter a valid answer")
+elif size == "M":
+     want_roni=input("Peperoni Y / N ?")
+     if want_roni == "Y":
+        print ("Your total is $22")
+     elif(want_roni == "N"):
+         print("Your total is $20")
+     else:
+         print("enter a valid answer")
+elif size == "L":
+     want_roni=input("Peperoni Y / N ?")
+     if want_roni == "Y":
+        print ("Your total is $27")
+     elif(want_roni == "N"):
+         print("Your total is $25")
+     else:
+         print("enter a valid answer")
+
 else:
-    print("not")
+    print("enter a valid size!")
