17842a882d7a565d2eaa1cdc4f03ba431b4d6add
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-01-17 15:28:38 -0500

    add asssigment and types

--- TypeScript/typeScriptBasics/sandbox.js
+++ TypeScript/typeScriptBasics/sandbox.js
@@ -1,6 +1,22 @@
-var character = "Zafer";
-console.log(character);
-var inputs = document.querySelectorAll("input");
-inputs.forEach(function (input) {
-  console.log(input);
-});
+var char = "zafer";
+// char = 20; //cannot reassign to a number. type script does type check
+// this check helps developer reduce the error
+var myArray = ["charmender", "balbasaur"];
+//myArray.push(2); // gives an error Argument of type 'number' is not assignable to parameter of type 'string'
+myArray.push("NEW POKEMON"); // This is ok!
+//if you define the array that contains strings and numbers only then you can push number or any type in it
+var mixedArray = [1, "one", 2, "two"];
+mixedArray.push(3);
+mixedArray.push("three");
+//same rule applies for the objects as well
+var myInfo = {
+    name: "zafer",
+    lastName: "ozcan",
+    age: 30
+};
+myInfo.age = 29; /// ththis is fine
+// myInfo.age = "thirty"; // this invalid
+var mixed = []; // this enables us to initialize an empty array then we can push string and number types
+mixed.push("hello");
+mixed.push(20);
+// mixed.push(true); // this is not allowed
--- TypeScript/typeScriptBasics/sandbox.ts
+++ TypeScript/typeScriptBasics/sandbox.ts
@@ -1,9 +1,32 @@
-const character = "Zafer";
+let char = "zafer";
 
-console.log(character);
+// char = 20; //cannot reassign to a number. type script does type check
+// this check helps developer reduce the error
 
-const inputs = document.querySelectorAll("input");
+let myArray = ["charmender", "balbasaur"];
 
-inputs.forEach((input) => {
-  console.log(input);
-});
+//myArray.push(2); // gives an error Argument of type 'number' is not assignable to parameter of type 'string'
+myArray.push("NEW POKEMON"); // This is ok!
+
+//if you define the array that contains strings and numbers only then you can push number or any type in it
+
+let mixedArray = [1, "one", 2, "two"];
+mixedArray.push(3);
+mixedArray.push("three");
+
+//same rule applies for the objects as well
+
+let myInfo = {
+  name: "zafer",
+  lastName: "ozcan",
+  age: 30,
+};
+
+myInfo.age = 29; /// ththis is fine
+// myInfo.age = "thirty"; // this invalid
+
+let mixed: (String | Number)[] = []; // this enables us to initialize an empty array then we can push string and number types
+
+mixed.push("hello");
+mixed.push(20);
+// mixed.push(true); // this is not allowed
