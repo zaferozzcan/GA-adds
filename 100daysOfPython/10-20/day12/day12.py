2730a314caf7ddb3dcdf1471018305a2a7733bcc
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-01-20 23:33:43 -0500

    number guessing game

--- 100daysOfPython/10-20/day12/day12.py
+++ 100daysOfPython/10-20/day12/day12.py
@@ -1,7 +1,16 @@
-enemies = 1
+import radnom
 
-def check_boundry():
-    enemies=2
-    print(enemies)
+def def_level():
+    level  = input("Choose a difficulty.Type 'easy' or 'hard': ")
+    if level.lower() == "easy":
+        return 10
+    elif level.lower() == "hard":
+        return 5
+    else:
+        print("Invalid entry")
+        def_level()
 
-check_boundry()
+difficulty = def_level()
+
+def random_number():
+    random_number = 
