2a9b53701c7305223d9953d16a48e486ac7b4bd0
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-01-22 18:14:38 -0500

    add path animation to the logo

--- Framer-Motion/PizzaJoint/src/components/Header.js
+++ Framer-Motion/PizzaJoint/src/components/Header.js
@@ -6,6 +6,15 @@ const svgVariants = {
   visible: { rotate: 0, transition: { duration: 1 } },
 };
 
+const pathVariant = {
+  hidden: { opacity: 0, pathLength: 0 },
+  visible: {
+    opacity: 1,
+    pathLength: 1,
+    transition: { duration: 2, ease: "easeInOut" },
+  },
+};
+
 const Header = () => {
   return (
     <header>
@@ -18,11 +27,16 @@ const Header = () => {
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 100 100"
         >
-          <path
+          <motion.path
+            variants={pathVariant}
             fill="none"
             d="M40 40 L80 40 C80 40 80 80 40 80 C40 80 0 80 0 40 C0 40 0 0 40 0Z"
           />
-          <path fill="none" d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z" />
+          <motion.path
+            variants={pathVariant}
+            fill="none"
+            d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z"
+          />
         </motion.svg>
       </div>
       <motion.div
