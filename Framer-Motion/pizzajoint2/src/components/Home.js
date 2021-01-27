66c15d23f8367dffd0c07ec7006370bc8142de1a
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-01-26 23:35:04 -0500

    add yoyo

--- Framer-Motion/pizzajoint2/src/components/Home.js
+++ Framer-Motion/pizzajoint2/src/components/Home.js
@@ -2,25 +2,29 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
+const buttonVariants = {
+  visible: {
+    transition: {
+      duration: Infinity,
+    },
+  },
+  hover: {
+    scale: 1.1,
+    transition: {
+      yoyo: 100,
+    },
+  },
+};
+
 const Home = () => {
   return (
     <motion.div className="home container">
-      <motion.h2
-        initial={{ opacity: 0 }}
-        animate={{
-          type: "zoomIn",
-          opacity: 1,
-          fontSize: 80,
-          color: "#ff2994",
-          transition: { duration: 3.1, ease: "easeInOut" },
-        }}
-      >
-        Welcome to Pizza Joint
-      </motion.h2>
+      <motion.h2>Welcome to Pizza Joint</motion.h2>
       <Link to="/base">
         <motion.button
-          initial={{ opacity: 0 }}
-          animate={{ scale: 1.5, opacity: 1, transition: { delay: 2.1 } }}
+          variants={buttonVariants}
+          animate="visible"
+          whileHover="hover"
         >
           Create Your Pizza
         </motion.button>
