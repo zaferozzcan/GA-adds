37ba02df35b1e4bcc2ab74eb98df544a50132299
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-01-28 21:50:38 -0500

    can read name list for the invotation

--- 100daysOfPython/21-30/day25/day25.py
+++ 100daysOfPython/21-30/day25/day25.py
@@ -12,6 +12,14 @@
 #     print(content)
 
 
-with open("zafer.txt", "a") as file:
+# with open("zafer.txt", "a") as file:
     
-    file.write("\nI am a line written by the write method!")
+#     file.write("\nI am a line written by the write method!")
+
+
+
+with open("names.txt") as names_list:
+    names = names_list.readlines()
+   
+
+with open("startig_file.txt")
