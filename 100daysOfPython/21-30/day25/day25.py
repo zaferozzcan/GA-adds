71589e291bbb70fc220f3fe1620d4d6e6c670718
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-01-28 21:23:03 -0500

    can write into a file with w and a mode

--- 100daysOfPython/21-30/day25/day25.py
+++ 100daysOfPython/21-30/day25/day25.py
@@ -7,6 +7,11 @@
 # file.close()
 
 
-with open("zafer.txt") as file:
-    content = file.read()
-    print(content)
+# with open("zafer.txt") as file:
+#     content = file.read()
+#     print(content)
+
+
+with open("zafer.txt", "a") as file:
+    
+    file.write("\nI am a line written by the write method!")
