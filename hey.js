3ab8c74bb10a4c1860eb0ead61ba535aeb193df2
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-02-02 19:12:35 -0500

    add challange

--- hey.js
+++ hey.js
@@ -39,12 +39,34 @@
 //   ])
 // );
 
-function mutateTheArray(n, a) {
-  let new_array = [];
-  for (let i = 0; i < a.length; i++) {
-    new_array[i] = (a[i - 1] ? a[i - 1] : 0) + a[i] + (a[i + 1] ? a[i + 1] : 0);
-  }
-  return new_array;
-}
+// function mutateTheArray(n, a) {
+//   let new_array = [];
+//   for (let i = 0; i < a.length; i++) {
+//     new_array[i] = (a[i - 1] ? a[i - 1] : 0) + a[i] + (a[i + 1] ? a[i + 1] : 0);
+//   }
+//   return new_array;
+// }
 
-console.log(mutateTheArray(5, [4, 0, 1, -2, 3]));
+// console.log(mutateTheArray(5, [4, 0, 1, -2, 3]));
+
+let a = [1, 2, 3];
+let b = [1, 2, 3];
+
+function countTinyPairs(a, b, k) {
+  let tiny_count = 0;
+  let concat_array = [...a, ...b];
+  let left = 0;
+  let right = concat_array.length - 1;
+  for (let i = 0; i < a.length; i++) {
+    let left_number = concat_array[left];
+    let right_number = concat_array[right];
+    let concated_numbers = Number(String(left_number) + String(right_number));
+    left++;
+    right--;
+    if (concated_numbers < k) {
+      tiny_count++;
+    }
+  }
+  return tiny_count;
+}
+console.log(countTinyPairs(a, b, 31));
