1455d02db0fc530847aa5da305f1b1fae2435d51
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-02-04 20:17:09 -0500

    aad codesignal challange

--- hey.js
+++ hey.js
@@ -1,9 +1,14 @@
-function adjacentElementsProduct(inputArray) {
-  let max = inputArray[0] * inputArray[1];
-  for (let i = 1; i < inputArray.length; i++) {
-    if (inputArray[i + 1] == undefined) break;
-    max = Math.max(max, inputArray[i] * inputArray[i + 1]);
+function makeArrayConsecutive2(statues) {
+  let freq_statues = {};
+  let counter = 0;
+  for (let i = 0; i < statues.length; i++) {
+    freq_statues[statues[i]] = (freq_statues[statues[i]] | 0) + 1;
   }
-  return max;
+  for (let i = Math.min(...statues); i < Math.max(...statues) + 1; i++) {
+    if (freq_statues[i] === undefined) counter++;
+  }
+  return counter;
 }
-console.log(adjacentElementsProduct([-23, 4, -3, 8, -12]));
+statues = [6, 2, 3, 8];
+
+console.log(makeArrayConsecutive2(statues));
