6e110406a6714ac2b06eabbaf29676fcec1637c4
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-02-06 16:10:58 -0500

    array changesol

--- hey.js
+++ hey.js
@@ -5,32 +5,16 @@
 // //   let top = "*".repeat(borderLen);
 // //   let bottom = "*".repeat(borderLen);
 
-// //   return [top, ...picture.map((item) => `*${item}*`), bottom];
-// // }
-
-// let addBorder = (picture) => [
-//   picture[0].replace(/./g, "*") + "**",
-//   ...picture.map((arr) => `*${arr}*`),
-//   picture[0].replace(/./g, "*") + "**",
-// ];
-
-// console.log(addBorder(picture));
-
-function areSimilar(a, b) {
-  const ad = a.filter((v, i) => v != b[i]);
-  console.log(ad);
-  const bd = b.filter((v, i) => v != a[i]);
-  return (
-    ad.length == 0 || (ad.length == 2 && ad.join("") == bd.reverse().join(""))
-  );
+function arrayChange(inputArray) {
+  let count = 0;
+  for (let i = 1; i < inputArray.length; i++) {
+    if (inputArray[i] <= inputArray[i - 1]) {
+      let diff = inputArray[i - 1] - inputArray[i] + 1;
+      inputArray[i] += diff;
+      count += diff;
+    }
+  }
+  return count;
 }
 
-console.log(areSimilar([1, 1, 2], [1, 2, 1]));
-
-let a = [1, 1, 2];
-let b = [1, 2, 1];
-
-let ax = a.filter((item, index) => item != b[index]);
-let bx = b.filter((item, index) => item != a[index]);
-
-console.log(ax.join("") == bx.reverse().join("") ? true : false);
+console.log(arrayChange([1, 1, 1]));
