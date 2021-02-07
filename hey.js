44c8e6f0b7c3f1df4532bca74e5088b27d7e6f3b
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-02-07 12:23:49 -0500

    reduce the length in input array to get it working

--- hey.js
+++ hey.js
@@ -7,12 +7,10 @@
 
 function arrayChange(inputArray) {
   let count = 0;
-  for (let i = 1; i < inputArray.length; i++) {
-    if (inputArray[i] <= inputArray[i - 1]) {
-      let diff = inputArray[i - 1] - inputArray[i] + 1;
-      inputArray[i] += diff;
-      count += diff;
-    }
+  for (let i = 0; i < inputArray.length - 1; i++) {
+    let diff = inputArray[i] - inputArray[i + 1] + 1;
+    inputArray[i + 1] += diff;
+    count += diff;
   }
   return count;
 }
