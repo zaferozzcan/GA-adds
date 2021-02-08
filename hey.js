e1aec8ebb6ced406690ed0eefa780753745fdd43
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-02-08 17:34:49 -0500

    add panlindrome check shortest approach

--- hey.js
+++ hey.js
@@ -1,16 +1,12 @@
 function palindromeRearranging(inputString) {
-  let freq = {};
-  for (let i = 0; i < inputString.length; i++) {
-    freq[inputString[i]] = (freq[inputString[i]] | 0) + 1;
-  }
-  var count = 0;
-  for (let key in freq) {
-    if (freq[key] % 2 != 0) {
-      count++;
-    }
-    if (count > 1) return false;
-  }
+  console.log(
+    inputString
+      .split("")
+      .sort()
+      .join("")
+      .replace(/(\w)\1/g, "")
+  );
 }
 let str = "abbcabb";
 
-console.log(palindromeRearranging(str));
+// console.log(palindromeRearranging(str));
