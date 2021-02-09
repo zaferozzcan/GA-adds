a8b825d3f479ae7db86e84ad9dfbfce8b8499729
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-02-09 15:09:00 -0500

    add a shorter solution to check operator challange

--- hey.js
+++ hey.js
@@ -1,21 +1,5 @@
-let matrix = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-];
-
-function rotate(matrix) {
-  for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < i; j++) {
-      matrix[i][j] ^= matrix[j][i];
-      matrix[j][i] ^= matrix[i][j];
-      matrix[i][j] ^= matrix[j][i];
-    }
-  }
-  for (let i of matrix) {
-    console.log(i);
-  }
-  console.log(matrix);
+function heck(a, b, c) {
+  return [..."+-*/"].some((op) => eval(`${a} ${op} ${b}`) == c);
 }
 
-rotate(matrix);
+console.log(heck(2, 3, 5));
