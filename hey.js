c11ece97e494e652c9faba0ad54d1f6424e2ad19
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-02-08 20:33:02 -0500

    prototype based inheritence

--- hey.js
+++ hey.js
@@ -1,7 +1,13 @@
-function greeting() {
-  return function sayHi() {
-    console.log("Hiey");
-  };
+
+function Horse(name, breed) {
+  this.name = name;
+  this.breed = breed
 }
 
-greeting()();
+Horse.prototype.getDetails = function(){
+  console.log(`Name:${this.name}   Breed:${this.breed}`);
+}
+
+const horse1 = new Horse("Grand Ekinoks", "Throghbreed");
+
+horse1.getDetails();
