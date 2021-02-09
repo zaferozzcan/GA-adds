063c435a314bc257ff50ed984897c8339616c822
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-02-09 00:37:41 -0500

    add altering aort

--- hey.js
+++ hey.js
@@ -1,11 +1,28 @@
-function lateRide(n) {
-  var hours = (n / 60) | 0,
-    minutes = n % 60;
-  console.log(hours);
-  console.log(minutes);
-  return (
-    ((hours / 10) | 0) + (hours % 10) + ((minutes / 10) | 0) + (minutes % 10)
-  );
-}
+/*
+Given an integer n and an array a of length n, your task is to apply the following mutation to a:
 
-lateRide(50);
+Array a mutates into a new array b of length n.
+For each i from 0 to n - 1, b[i] = a[i - 1] + a[i] + a[i + 1].
+If some element in the sum a[i - 1] + a[i] + a[i + 1] does not exist, it should be set to 0. For example, b[0] should be equal to 0 + a[0] + a[1].
+*/
+function alternatingSort(a) {
+  let new_array = [];
+  let left = 0;
+  let right = a.length - 1;
+  while (left <= right) {
+    new_array.push(a[left]);
+    new_array.push(a[right]);
+    left++;
+    right--;
+  }
+  new_array.pop();
+  console.log(new_array);
+  for (let i = 0; i < new_array.length; i++) {
+    if (new_array[i + 1] <= new_array[i]) return false;
+  }
+
+  return true;
+}
+console.log(
+  alternatingSort([-92, -23, 0, 45, 89, 96, 99, 95, 89, 41, -17, -48])
+);
