a051cdd22302cff55d9eb8bec4201280ca29f044
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-02-12 22:13:41 -0500

    can return map items

--- hey.js
+++ hey.js
@@ -7,15 +7,11 @@ let events = [
 
 function shopInAndOutEvents(events) {
   if (events.length % 2 !== 0) return false;
-  let freqMap = {};
+  let obj = {};
   events.forEach((item) => {
-    if (freqMap[item[0]] === (freqMap[item[0]] == "out" ? "in" : "out")) {
-      return false;
+    if (!obj[item[0]]) {
+      return;
     }
-    freqMap[item[0]] = item[1];
-    console.log(freqMap);
   });
-
-  return true;
 }
 console.log(shopInAndOutEvents(events));
