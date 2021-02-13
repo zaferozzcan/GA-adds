4d16234515259634f20883214260dcc325a6bf04
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-02-12 20:23:15 -0500

    add countTiny

--- hey.js
+++ hey.js
@@ -1,35 +1,12 @@
-// let string1 = "I like t";
-// let string2 = "e turtle";
-// let count = 0;
-// let finalString = "";
-
-// string1.split("").forEach((item) => {
-
-// });
-
-function solution(N) {
-  // write your code in JavaScript (Node.js 8.9.4)
-
-  let stringM = String(N);
-  let stringN = N > 0 ? String(N) : String(N).slice(1);
-  if (stringM.length == 1) {
-    if (N >= 0) {
-      return Number("5" + stringM);
-    } else {
-      return Number(stringM + "5") * -1;
+function countTinyPairs(a, b, k) {
+  let count = 0;
+  for (let i = 0; i < a.length; i++) {
+    console.log(eval(`${a[i]}` + `${b[a.length - 1 - i]}`));
+    if (eval(`${a[i]}` + `${b[a.length - 1 - i]}`) < k) {
+      count++;
     }
   }
-
-  let first = Number("5" + stringN);
-
-  let result = [first];
-  for (let i = 1; i < stringN.length; i++) {
-    let slice1 = stringN.slice(0, i);
-    let slice2 = stringN.slice(i);
-    Number(slice1 + "5" + slice2) && result.push(Number(slice1 + "5" + slice2));
-  }
-
-  return N > 0 ? Math.max(...result) : Math.min(...result) * -1;
+  return count;
 }
 
-console.log(solution(-999));
+console.log(countTinyPairs([16, 1, 4, 2, 14], [7, 11, 2, 0, 15], 743));
