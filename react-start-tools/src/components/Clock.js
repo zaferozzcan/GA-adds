64c29ca6fe2f638ef413be2e10c4cea74177de05
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-02-18 11:59:02 -0500

    add coordinates tsuff

--- react-start-tools/src/components/Clock.js
+++ react-start-tools/src/components/Clock.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from "react";
-let timeStamp = new Date().toLocaleTimeString();
-function Clock() {
-  const [time, setTime] = useState();
 
-  function timeChange() {
-    setTime(timeStamp);
-  }
-  console.log(time);
-  return <div></div>;
+function Clock() {
+  const [time, setTime] = useState("0");
+
+  useEffect(() => {
+    setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 1000);
+  });
+
+  return <div>{time}</div>;
 }
 
 export default Clock;
