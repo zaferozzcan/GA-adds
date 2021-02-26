934e68ad3e3b9f329e59eed4b55a0fb77cda1ba0
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-02-25 22:58:02 -0500

    add mirror bits

--- hey.js
+++ hey.js
@@ -1,11 +1,3 @@
-function rangeBitCount(a, b) {
-  let count = 0;
-  for (let i = a; i < b + 1; i++) {
-    var binaryRep = Number(i).toString(2).split("");
-    for (let j = 0; j < binaryRep.length; j++) {
-      if (binaryRep[j] == "1") count++;
-    }
-  }
-  return count;
+function mirrorBits(a) {
+  return parseInt([...a.toString(2)].reverse().join(""), 2);
 }
-console.log(rangeBitCount(2, 7));
