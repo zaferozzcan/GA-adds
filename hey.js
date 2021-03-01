374512be612e4234225be8c3805506016a831508
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-02-28 19:50:37 -0500

    start lineup

--- hey.js
+++ hey.js
@@ -1,10 +1,1 @@
-function magicalWell(a, b, n) {
-  let sum = 0;
-  for (let i = 0; i < n; i++) {
-    sum += a * b;
-    a++;
-    b++;
-  }
-  return sum;
-}
-console.log(magicalWell(1, 2, 2));
+function lineUp(commands) {}
