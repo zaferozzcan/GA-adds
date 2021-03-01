44c36a085b52575dd9ce8891d82d1f83e79a32c3
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-02-28 19:53:40 -0500

    add line up problem'

--- hey.js
+++ hey.js
@@ -1,1 +1,11 @@
-function lineUp(commands) {}
+function lineUp(commands) {
+  var result = 0,
+    same = true;
+  for (var i = 0; i < commands.length; i++) {
+    var command = commands[i];
+    if (command == "L" || command == "R") {
+      same = !same;
+    }
+    if (same) result++;
+  }
+}
