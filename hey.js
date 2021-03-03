cc32bb25fe8774be6306f27e2313c769c1847f50
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-03-03 17:41:33 -0500

    a w c

--- hey.js
+++ hey.js
@@ -1,17 +1,3 @@
-// function additionWithoutCarrying(param1, param2) {
-//   var result = 0,
-//     num = 1;
-//   while (param1 + param2 > 0) {
-//     num *= 10;
-//     result += (param1 + param2) % num;
-//     param1 -= param1 % num;
-//     param2 -= param2 % num;
-//   }
-//   return result;
-// }
-
-// console.log(additionWithoutCarrying(456, 1734));
-
 function additionWithoutCarrying(param1, param2) {
   param1 = String(param1);
   param2 = String(param2);
