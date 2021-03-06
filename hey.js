25662fb2e2bbce7837ca8ba103d4bb426f1d9789
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-03-06 13:00:47 -0500

    add increase roundness

--- hey.js
+++ hey.js
@@ -1,14 +1,18 @@
-function appleBoxes(k) {
-  let even_sum = 0;
-  let odd_sum = 0;
-  for (let i = 1; i < k + 1; i++) {
-    if (i % 2 == 0) {
-      even_sum += i * i;
-    } else {
-      odd_sum += i * i;
-    }
-  }
-  return even_sum - odd_sum;
-}
+// function appleBoxes(k) {
+//   let even_sum = 0;
+//   let odd_sum = 0;
+//   for (let i = 1; i < k + 1; i++) {
+//     if (i % 2 == 0) {
+//       even_sum += i * i;
+//     } else {
+//       odd_sum += i * i;
+//     }
+//   }
+//   return even_sum - odd_sum;
+// }
 
-console.log(appleBoxes(5));
+// console.log(appleBoxes(5));
+
+function increaseNumberRoundness(n) {}
+
+console.log(increaseNumberRoundness(902200100));
