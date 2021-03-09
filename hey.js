04fae96855b4387365e57a219bdcdb4da2641cd4
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-03-08 20:23:27 -0500

    add  rounserd chhlge

--- hey.js
+++ hey.js
@@ -1,28 +1,5 @@
-// function appleBoxes(k) {
-//   let even_sum = 0;
-//   let odd_sum = 0;
-//   for (let i = 1; i < k + 1; i++) {
-//     if (i % 2 == 0) {
-//       even_sum += i * i;
-//     } else {
-//       odd_sum += i * i;
-//     }
-//   }
-//   return even_sum - odd_sum;
-// }
+function rounders(n) {}
 
-// console.log(appleBoxes(5));
+console.log(rounders(1234));
 
-function increaseNumberRoundness(n) {
-  const parts = n.toString().split("").reverse();
-  let state = false;
-  for (let part of parts) {
-    if (part !== "0") state = true;
-    else if (state && part === "0") {
-      return true;
-    }
-  }
-  return false;
-}
-
-console.log(increaseNumberRoundness(888));
+console.log(1234 % 100);
