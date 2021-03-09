2b8f052dc9f4c7c72fa326764c0fcd71e1abb034
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-03-08 20:32:38 -0500

    bug on roundness

--- hey.js
+++ hey.js
@@ -1,5 +1,16 @@
-function rounders(n) {}
+function rounders(n) {
+  let mod_num = 10;
+  let lim = 5;
+  while (lim < n) {
+    if (n % mod_num > lim) {
+      n = n + (10 - (n % mod_num));
+    } else {
+      n = n - (n % mod_num);
+    }
+    mod_num = mod_num * 10;
+    lim = lim * 10;
+  }
+  return n;
+}
 
-console.log(rounders(1234));
-
-console.log(1234 % 100);
+console.log(rounders(1634));
