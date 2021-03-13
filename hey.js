89535764d2c889266ede022ff1eb6b90207134bb
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-03-12 21:17:42 -0500

    rounders final sol

--- hey.js
+++ hey.js
@@ -1,12 +1,10 @@
-function rounders(n) {
-  function rounders(value) {
-    var r = 0;
-    while (value > 10) {
-      value = Math.round(value / 10);
-      r++;
-    }
-    return value * Math.pow(10, r);
+function rounders(value) {
+  var r = 0;
+  while (value > 10) {
+    value = Math.round(value / 10);
+    r++;
   }
+  return value * Math.pow(10, r);
 }
 
 console.log(rounders(1445));
