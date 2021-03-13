67ffaf6e421d286193ff0210e56b567b899c7f1c
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-03-13 14:40:49 -0500

    add max area start

--- hey.js
+++ hey.js
@@ -2,9 +2,13 @@
 //   return m - (m % n);
 // }
 
-function phoneCall(min1, min2_10, min11, s) {
-  if (s < min1) return 0;
-  if (s == min1) return 1;
-  if (s - min1 <= min2_10 * 9) return 1 + Math.floor((s - min1) / min2_10);
-  return Math.floor((s - min1 - min2_10 * 9) / min11 + 10);
-}
+// function phoneCall(min1, min2_10, min11, s) {
+//   if (s < min1) return 0;
+//   if (s == min1) return 1;
+//   if (s - min1 <= min2_10 * 9) return 1 + Math.floor((s - min1) / min2_10);
+//   return Math.floor((s - min1 - min2_10 * 9) / min11 + 10);
+// }
+
+var maxArea = function (height) {};
+
+console.log([1, 8, 6, 2, 5, 4, 8, 3, 7]);
