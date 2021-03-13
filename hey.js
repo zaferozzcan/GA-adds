69c45c2002c29549b24b3582fc316a007919986a
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-03-12 21:15:00 -0500

    closing the rounders

--- hey.js
+++ hey.js
@@ -1,1 +1,16 @@
-function rounders(n) {}
+function rounders(n) {
+  n = String(n).split("");
+  for (let i = n.length; i > 0; i--) {
+    if (n[i] >= 5) {
+      n[i] = 0;
+      n[i - 1] ? n[i - 1] + 1 : n[i - 1];
+    } else {
+      n[i] = 0;
+    }
+  }
+  n.pop();
+
+  return Number(n.map((item) => String(item)).join(""));
+}
+
+console.log(rounders(1234));
