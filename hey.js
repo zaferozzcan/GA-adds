6e479b7c3ea678250943c798c7fba7cf570bbc00
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-03-13 14:36:35 -0500

    solved phone call chal

--- hey.js
+++ hey.js
@@ -1,3 +1,10 @@
-function candies(n, m) {
-  return m - (m % n);
+// function candies(n, m) {
+//   return m - (m % n);
+// }
+
+function phoneCall(min1, min2_10, min11, s) {
+  if (s < min1) return 0;
+  if (s == min1) return 1;
+  if (s - min1 <= min2_10 * 9) return 1 + Math.floor((s - min1) / min2_10);
+  return Math.floor((s - min1 - min2_10 * 9) / min11 + 10);
 }
