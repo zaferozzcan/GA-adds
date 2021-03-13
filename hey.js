fd871a8ee48afffe0ce4a0f8aaa359c1d70df3a1
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-03-12 21:00:11 -0500

    start rounders again

--- hey.js
+++ hey.js
@@ -1,17 +1,1 @@
-function rounders(n) {
-  let mod_num = 10;
-  let lim = 5;
-  while (lim < n) {
-    if (n % mod_num > lim) {
-      n = n + (10 - (n % mod_num));
-    } else {
-      n = n - (n % mod_num);
-    }
-    mod_num = mod_num * 10;
-    lim = lim * 10;
-  }
-  return n;
-}
-
-console.log(rounders(1634));
-// 
+function rounders(n) {}
