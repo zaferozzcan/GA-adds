5d90bd2c431df14f1c82c0aaa607742edd9306f4
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-03-12 21:16:43 -0500

    solve rounders

--- hey.js
+++ hey.js
@@ -1,16 +1,12 @@
 function rounders(n) {
-  n = String(n).split("");
-  for (let i = n.length; i > 0; i--) {
-    if (n[i] >= 5) {
-      n[i] = 0;
-      n[i - 1] ? n[i - 1] + 1 : n[i - 1];
-    } else {
-      n[i] = 0;
+  function rounders(value) {
+    var r = 0;
+    while (value > 10) {
+      value = Math.round(value / 10);
+      r++;
     }
+    return value * Math.pow(10, r);
   }
-  n.pop();
-
-  return Number(n.map((item) => String(item)).join(""));
 }
 
-console.log(rounders(1234));
+console.log(rounders(1445));
