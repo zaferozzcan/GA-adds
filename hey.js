7f6b736e21909a690562b71c5ffe661412b1c8ea
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-03-16 15:41:48 -0400

    start two sum stuff

--- hey.js
+++ hey.js
@@ -1,56 +1,23 @@
-// function candies(n, m) {
-//   return m - (m % n);
-// }
-
-// function phoneCall(min1, min2_10, min11, s) {
-//   if (s < min1) return 0;
-//   if (s == min1) return 1;
-//   if (s - min1 <= min2_10 * 9) return 1 + Math.floor((s - min1) / min2_10);
-//   return Math.floor((s - min1 - min2_10 * 9) / min11 + 10);
-// }
-
-// var maxArea = function (nums) {
-//   let areas = [];
-//   let left = 0;
-//   let right = nums.length - 1;
-//   while (left < right) {
-//     let h = Math.min(nums[left], nums[right]);
-//     let w = right - left;
-//     areas.push(h * w);
-//     if (nums[left + 1] > Math.min(nums[left], nums[right])) {
-//       left++;
-//     } else {
-//       right--;
-//     }
-//   }
-//   return Math.max(...areas);
-// };
-// console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
-//
-
-function isIPv4Address(s) {
-  let s_arr = s.split(".");
-  let ret = true;
-
-  if (s_arr.length < 4) {
-    ret = false;
-  }
-  if (s_arr.includes("")) {
-    ret = false;
-  }
-
-  if (s_arr.pop() != "0") {
-  } else if (s_arr.pop() != "1") {
-  } else {
-    ret = false;
-  }
-  s_arr.map((item) => {
-    if (Number(item >= 0) && Number(item <= 255)) {
-    } else {
-      ret = false;
+function twoSum(nums, target) {
+  let idx = [];
+  let newNums = nums.sort((a, b) => a - b);
+  let left = 0;
+  let right = nums.length - 1;
+  while (left < right) {
+    let sum = newNums[left] + newNums[right];
+    if (sum == target) {
+      idx.push(newNums[left]);
+      idx.push(newNums[right]);
     }
-  });
-  return ret;
+    if (sum > target) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+  console.log(nums);
+  console.log(newNums);
+  return [nums.indexOf(idx[0]), nums.indexOf(idx[1])];
 }
 
-console.log(isIPv4Address("172.16.254.10"));
+console.log(twoSum([3, 2, 4], 6));
