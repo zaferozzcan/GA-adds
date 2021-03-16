0b9c0d1996e508c6fdf9b9b901e5666f4ef8cff9
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-03-16 16:03:56 -0400

    solution twoSum

--- hey.js
+++ hey.js
@@ -1,23 +1,15 @@
 function twoSum(nums, target) {
-  let idx = [];
-  let newNums = nums.sort((a, b) => a - b);
-  let left = 0;
-  let right = nums.length - 1;
-  while (left < right) {
-    let sum = newNums[left] + newNums[right];
-    if (sum == target) {
-      idx.push(newNums[left]);
-      idx.push(newNums[right]);
-    }
-    if (sum > target) {
-      right--;
-    } else {
-      left++;
+  let numObject = {};
+  for (var i = 0; i < nums.length; i++) {
+    let thisNum = nums[i];
+    numObject[thisNum] = i;
+  }
+  for (var i = 0; i < nums.length; i++) {
+    let diff = target - nums[i];
+    if (numObject.hasOwnProperty(diff) && numObject[diff] !== i) {
+      return [i, numObject[diff]];
     }
   }
-  console.log(nums);
-  console.log(newNums);
-  return [nums.indexOf(idx[0]), nums.indexOf(idx[1])];
 }
 
 console.log(twoSum([3, 2, 4], 6));
