d564fc60f004d6e056fd43f4a7f562dbdd76908a
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-03-20 14:35:01 -0400

    add ksalksa

---
+++ code-challenges/leetcode/medianOfTwoSortedArr.js
@@ -0,0 +1,6 @@
+var findMedianSortedArrays = function (nums1, nums2) {
+  let merged = [...nums1, ...nums2];
+  return merged;
+};
+
+console.log(findMedianSortedArrays([1, 3], [2]));
--- hey.js
+++ hey.js
@@ -1,7 +1,13 @@
-function twoSum(nums, target) {
-  let storage = {};
-  for (let [index, num] of nums.entries()) {
-    if (storage[num] !== undefined) return [storage[num], index];
-    storage[target - num] = index;
+var findMedianSortedArrays = function (nums1, nums2) {
+  let merged = [...nums1, ...nums2].sort((a, b) => a - b);
+  if (merged.length % 2 == 0) {
+    let mid1 = merged[merged.length / 2];
+    let mid2 = merged[merged.length / 2 - 1];
+    console.log(mid1, mid2);
+    return (mid1 + mid2) / 2;
+  } else {
+    return merged[Math.floor(merged.length / 2)];
   }
-}
+};
+
+console.log(findMedianSortedArrays([1, 3], [2, 4]));
