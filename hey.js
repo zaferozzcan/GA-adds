7e048aa88a2b2cd90f5c3c8bfce643fa686f5c94
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-03-20 14:02:15 -0400

    add iki numara

--- hey.js
+++ hey.js
@@ -1,15 +1,32 @@
+// function twoSum(nums, target) {
+//   let numObject = {};
+//   for (var i = 0; i < nums.length; i++) {
+//     let thisNum = nums[i];
+//     numObject[thisNum] = i;
+//   }
+//   for (var i = 0; i < nums.length; i++) {
+//     let diff = target - nums[i];
+//     if (numObject.hasOwnProperty(diff) && numObject[diff] !== i) {
+//       return [i, numObject[diff]];
+//     }
+//   }
+// }
+
+// console.log(twoSum([3, 3], 6));
+
 function twoSum(nums, target) {
-  let numObject = {};
+  var myDict = {};
+
   for (var i = 0; i < nums.length; i++) {
-    let thisNum = nums[i];
-    numObject[thisNum] = i;
+    if (nums[i] <= target) {
+      myDict[target - nums[i]] = i;
+    }
   }
-  for (var i = 0; i < nums.length; i++) {
-    let diff = target - nums[i];
-    if (numObject.hasOwnProperty(diff) && numObject[diff] !== i) {
-      return [i, numObject[diff]];
+  for (let key in myDict) {
+    if (myDict[target - key]) {
+      return [myDict[key], myDict[target - key]];
     }
   }
 }
 
-console.log(twoSum([3, 2, 4], 6));
+console.log(twoSum([3, 3], 6));
