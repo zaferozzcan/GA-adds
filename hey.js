3bebfad3b8fb798c4019a09ec92d9adb8e486f42
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-04-09 18:06:33 -0400

    add missing number

--- hey.js
+++ hey.js
@@ -1,11 +1,16 @@
-function findSingle(arr) {
+function missingNumber(arr) {
   let freq = {};
-  for (let [index, num] of arr.entries()) {
+  let current = Math.min(...arr) > 1 ? 1 : 1;
+  let max = arr.length;
+  let missing = [];
+  for (let num of arr) {
     freq[num] = (freq[num] | 0) + 1;
   }
-  for (let key of Object.keys(freq)) {
-    if (freq[key] === 1) return key;
+
+  for (let i = 1; i <= arr.length; i++) {
+    if (freq[i] === undefined) missing.push(i);
   }
+  return missing;
 }
 
-console.log(findSingle([3, 3, 8]));
+console.log(missingNumber([1, 1]));
