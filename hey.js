b55be6aaa07afacccb017c771216ea07091d2300
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-04-09 16:08:18 -0400

    add ters cevirme

--- hey.js
+++ hey.js
@@ -1,2 +1,8 @@
-var numIslands = function (grid) {};
-//
+function reverseStr(str) {
+  let retStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    retStr += str[i];
+  }
+  return retStr;
+}
+console.log(reverseStr("zafer"));
