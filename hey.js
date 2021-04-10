841afba514399d7b4efd0a32fb37fce0ad1ee63f
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-04-09 22:02:21 -0400

    d and c pattern'

--- hey.js
+++ hey.js
@@ -1,16 +1,17 @@
-function missingNumber(arr) {
-  let freq = {};
-  let current = Math.min(...arr) > 1 ? 1 : 1;
-  let max = arr.length;
-  let missing = [];
-  for (let num of arr) {
-    freq[num] = (freq[num] | 0) + 1;
+// dvide and conquer
+function findElem(arr, target) {
+  let min = 0;
+  let max = arr.length - 1;
+  // loop
+  while (min < max) {
+    let middleIndex = Math.floor((min + max) / 2);
+    let middleNumber = arr[middleIndex];
+    if (middleNumber > target) {
+      max = middleIndex - 1;
+    } else if (middleNumber < target) {
+      min = middleIndex + 1;
+    } else return middleIndex;
   }
-
-  for (let i = 1; i <= arr.length; i++) {
-    if (freq[i] === undefined) missing.push(i);
-  }
-  return missing;
 }
 
-console.log(missingNumber([1, 1]));
+console.log(findElem([1, 3, 4, 5, 34, 56, 78, 99, 100], 99));
