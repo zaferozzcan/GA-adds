11106cd4293ccc73fc5531b076d382ac5dfc0188
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-04-10 13:10:08 -0400

    mutate teh arr

--- hey.js
+++ hey.js
@@ -1,14 +1,14 @@
-function maxSubArraySum(arr, num) {
-  let maxSum = 0;
-  for (let i = 0; i < num; i++) {
-    maxSum += arr[i];
+//b[i] = a[i - 1] + a[i] + a[i + 1].
+
+function mutateTheArray(n, a) {
+  let newArray = [];
+  for (let i = 0; i < n; i++) {
+    let num =
+      (a[i - 1] ? a[i - 1] : 0) + (a[i] ? a[i] : 0) + (a[i + 1] ? a[i + 1] : 0);
+    newArray.push(num);
+    num = 0;
   }
-  let tempSum = maxSum;
-  for (let i = num; i < arr.length; i++) {
-    tempSum = tempSum - arr[i - num] + arr[i];
-    maxSum = Math.max(maxSum, tempSum);
-  }
-  return maxSum;
+  return newArray;
 }
 
-console.log(maxSubArraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3));
+console.log(mutateTheArray(5, [4, 0, 1, -2, 3]));
