7370780d0533593091a26fb7a28c92dd6d43f243
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-04-11 14:25:46 -0400

    add max ara dizin

---
+++ code-challenges/leetcode/array_p53.js
@@ -0,0 +1,8 @@
+let arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+
+function maxSubArray(nums) {
+  let solution = nums[0];
+  console.log(solution);
+}
+
+console.log(maxSubArray(arr));
--- hey.js
+++ hey.js
@@ -1,15 +1,24 @@
-let arr = [2, 1, 3, 5, 3, 2];
+// let a = [1, 2, 3];
+// let b = [10, 20, 30, 40];
+// let v = 42;
 
-function firstDuplicate(arr) {
-  let freq = {};
-  for (let i = 0; i < arr.length; i++) {
-    if (freq[arr[i]] === undefined) {
-      freq[arr[i]] = 1;
-    } else {
-      return arr[i];
-    }
-  }
-  return -1;
-}
+// function sumOfTwo(a, b, v) {
+//   function diffObj(arr, target) {
+//     let freq = {};
+//     for (let i = 0; i < arr.length; i++) {
+//       freq[target - arr[i]] = (freq[target - arr[i]] | 0) + 1;
+//     }
+//     return freq;
+//   }
+//   let freq1 = diffObj(a, v);
 
-console.log(firstDuplicate(arr));
+//   return b.some((item) => freq1[item] != undefined);
+// }
+
+// console.log(sumOfTwo(a, b, v));
+
+let arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+
+function maxSubArray(nums) {}
+
+console.log(maxSubArray(arr));
