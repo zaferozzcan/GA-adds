1548ed43a31a371648bc57a1a71750da76134d30
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-04-11 14:31:39 -0400

    max ara dixin

--- hey.js
+++ hey.js
@@ -19,6 +19,13 @@
 
 let arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
 
-function maxSubArray(nums) {}
+function maxSubArray(nums) {
+  let solution = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    nums[i] = Math.max(nums[i], nums[i] + nums[i - 1]);
+    solution = Math.max(solution, nums[i]);
+  }
+  return solution;
+}
 
 console.log(maxSubArray(arr));
