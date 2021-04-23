82b3b0a8d33efa5a4781fbdefb1850c345978c30
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-04-22 20:43:27 -0400

    add diff app to the c un val

--- hey.js
+++ hey.js
@@ -38,4 +38,15 @@ function countUnique(arr) {
   return len;
 }
 
-console.log(countUnique([1, 2, 3, 4, 5, 5, 5, 6, 6, 6, 6, 7, 7]));
+function countUnique2(arr) {
+  let i = 0;
+  for (let j = 1; j <= arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return i;
+}
+
+console.log(countUnique2([1, 2, 3, 4]));
