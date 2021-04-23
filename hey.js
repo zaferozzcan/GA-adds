4a1380cb1d75d4b73be9202da26ae18789d74c50
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-04-22 20:58:36 -0400

    maxSubArr

--- hey.js
+++ hey.js
@@ -1,52 +1,14 @@
-let set = new Set([
-  1,
-  2,
-  3,
-  4,
-  4,
-  3,
-  2,
-  1,
-  1,
-  1,
-  1,
-  2,
-  2,
-  2,
-  3,
-  3,
-  3,
-  4,
-  4,
-  4,
-  4,
-]);
-
-function countUnique(arr) {
-  let left = 0;
-  let right = 0;
-  let len = 0;
-
-  while (right <= arr.length) {
-    if (arr[left] === arr[right]) {
-      right++;
-    } else {
-      left = right;
-      len++;
-    }
+function maxSubArraySum(arr, val) {
+  let maxSum = 0;
+  for (let i = 0; i < val; i++) {
+    maxSum += arr[i];
   }
-  return len;
+  let tempSum = maxSum;
+  for (let i = val; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - val] + arr[i];
+    maxSum = Math.max(tempSum, maxSum);
+  }
+  return maxSum;
 }
 
-function countUnique2(arr) {
-  let i = 0;
-  for (let j = 1; j <= arr.length; j++) {
-    if (arr[i] !== arr[j]) {
-      i++;
-      arr[i] = arr[j];
-    }
-  }
-  return i;
-}
-
-console.log(countUnique2([1, 2, 3, 4]));
+console.log(maxSubArraySum([1, 2, 3, 9, 4, 5, 5], 2));
