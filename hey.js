1be3e08f5d679757269fd6fc1fc2a0c8e141c314
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-04-23 20:14:43 -0400

    check dups

--- hey.js
+++ hey.js
@@ -21,22 +21,30 @@
 //   let right = 1;
 // }
 
-function sameFrequency(num1, num2) {
-  num1 = String(num1).split("");
-  num2 = String(num2).split("");
-  function freqMaker(arr) {
-    let freq = {};
-    for (let i = 0; i < arr.length; i++) {
-      freq[arr[i]] = (freq[arr[i]] | 0) + 1;
-    }
-    return freq;
-  }
-  let freq1 = freqMaker(num1);
-  let freq2 = freqMaker(num2);
-  for (let key in freq1) {
-    if (freq1[key] !== freq2[key]) return false;
-  }
-  return true;
+// function sameFrequency(num1, num2) {
+//   num1 = String(num1).split("");
+//   num2 = String(num2).split("");
+//   function freqMaker(arr) {
+//     let freq = {};
+//     for (let i = 0; i < arr.length; i++) {
+//       freq[arr[i]] = (freq[arr[i]] | 0) + 1;
+//     }
+//     return freq;
+//   }
+//   let freq1 = freqMaker(num1);
+//   let freq2 = freqMaker(num2);
+//   for (let key in freq1) {
+//     if (freq1[key] !== freq2[key]) return false;
+//   }
+//   return true;
+// }
+
+// console.log(sameFrequency(123, 331));
+
+function areThereDuplicates(arr) {
+  let set = new Set(arr);
+
+  return arr.length > set.size ? true : false;
 }
 
-console.log(sameFrequency(123, 331));
+console.log(areThereDuplicates(["a", "b", "b"]));
