625ec9720159ad89e20ce32ff15a35c21e60a102
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-04-25 14:33:55 -0400

    add mat rot

--- hey.js
+++ hey.js
@@ -253,15 +253,30 @@
 //   }
 //   return arr;
 // }
-function insertionSort(arr) {
-  for (var i = 1; i < arr.length; i++) {
-    var currentVal = arr[i];
-    for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
-      arr[j + 1] = arr[j];
+// function insertionSort(arr) {
+//   for (var i = 1; i < arr.length; i++) {
+//     var currentVal = arr[i];
+//     for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+//       arr[j + 1] = arr[j];
+//     }
+//     arr[j + 1] = currentVal;
+//   }
+//   return arr;
+// }
+
+// console.log(insertionSort([23, 4, 32, 56, 76, -1, 7]));
+
+function rotateMatrix(matrix) {
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      console.log(matrix[i][j]);
     }
-    arr[j + 1] = currentVal;
   }
-  return arr;
 }
 
-console.log(insertionSort([23, 4, 32, 56, 76, -1, 7]));
+let matrix = [
+  [1, 2],
+  [3, 4],
+];
+
+rotateMatrix(matrix);
