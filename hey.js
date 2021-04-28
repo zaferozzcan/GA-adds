3f9e227c17f3c13d91109de56b23e5a71950f797
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-04-28 17:02:26 -0400

    add debug eff in hey index page

--- hey.js
+++ hey.js
@@ -1,78 +1,20 @@
-class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
+let board = [
+  ["A", "B", "C", "E"],
+  ["S", "F", "C", "S"],
+  ["A", "D", "E", "E"],
+];
+
+let word = "ABCCED";
+
+function exist(board, word) {
+  let counter_index = 0;
+
+  while (counter_index < word.length) {
+    let target_letter = word[counter_index];
+    for (let i = 0; i < board.length; i++) {
+      for (let j = 0; j < board[i].length; j++) {
+        if()
+      }
+    }
   }
 }
-
-class SinglyLinkList {
-  constructor() {
-    this.length = 0;
-    this.head = null;
-    this.tail = null;
-  }
-  push(val) {
-    let newNode = new Node(val);
-    if (!this.tail) {
-      this.head = newNode;
-      this.tail = newNode;
-      this.length++; //
-    } else {
-      this.tail.next = newNode;
-      this.tail = newNode;
-      this.length++;
-    }
-    return this;
-  }
-  pop() {
-    if (!this.head) return undefined;
-    var current = this.head;
-    var newTail = current.next;
-    while (current.next) {
-      var newTail = current;
-      current = current.next;
-    }
-    this.tail = newTail;
-    this.tail.next = null;
-    this.length--;
-    return this;
-  }
-  shift() {
-    if (!this.head) return undefined;
-    var currentHead = this.head;
-    this.head = currentHead.next;
-    this.length--;
-    if (this.length === 0) {
-      this.tail == null;
-    }
-    return currentHead;
-  }
-  unshift(val) {
-    let newNode = new Node(val);
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = newNode;
-      this.length++;
-    } else {
-      let currentHead = this.head;
-      this.head = newNode;
-      this.head.next = currentHead;
-      this.length++;
-    }
-    return this;
-  }
-  get(index) {
-    if (index < 0 || this.length == 0) return null;
-    let counter = 0;
-    let current = this.head;
-  }
-}
-
-let myList = new SinglyLinkList();
-myList.push(10);
-myList.push(20);
-myList.push(30);
-myList.push(40);
-myList.shift();
-myList.unshift(10);
-console.log(myList);
