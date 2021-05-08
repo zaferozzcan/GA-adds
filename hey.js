3c2db43451f949fbc155000e31be21cf9bf020af
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-05-07 23:34:50 -0400

    add sasjalksj

--- hey.js
+++ hey.js
@@ -107,6 +107,23 @@
 
 // console.log(wordSubsets(A, B));
 
-const getMaxWaterContainer = (arr) => {};
+const getMaxWaterContainer = (arr) => {
+  let p1 = 0,
+    p2 = heights.length - 1,
+    maxArea = 0;
+
+  while (p1 < p2) {
+    const height = Math.min(heights[p1], heights[p2]);
+    const width = p2 - p1;
+    const area = height * width;
+    maxArea = Math.max(maxArea, area);
+    if (heights[p1] <= heights[p2]) {
+      p1++;
+    } else {
+      p2--;
+    }
+  }
+  return maxArea;
+};
 
 const height = [1, 8, 6, 2, 5, 4, 8, 3, 7];
