55c7a9e6f5997dae9a52cf10133bf4bc3427ae87
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-05-18 21:04:32 -0400

    comp full rest ap

--- VanillaJS/nodeling/app.js
+++ VanillaJS/nodeling/app.js
@@ -1,14 +1,23 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 app.get("/", function (req, res) {
   res.send(
-    "<form style='margin:100px 0 0 100px'><input type='text'></input><input type='text'></input><button>Calculate</button></form>"
+    "<form method='POST' style='margin:100px 0 0 100px'><input type='text' name='num1'></input><input type='text' name='num2'></input><button>Calculate</button></form>"
   );
 });
 
-app.post("/");
+app.post("/", function (req, res) {
+  let sum = Number(req.body.num1) + Number(req.body.num2);
+  res.send("your sum is " + `${sum}`);
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
