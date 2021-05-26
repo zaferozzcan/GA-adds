9d5ca143eb6a7ce4aa855a5201fc94996f304d9e
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-05-25 21:47:04 -0400

    add ma s arr

--- code-challenges/leetcode/array_p53.js
+++ code-challenges/leetcode/array_p53.js
@@ -10,3 +10,16 @@ function maxSubArray(nums) {
 }
 
 console.log(maxSubArray(arr));
+
+// let arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+
+// function maxSubArray(nums) {
+//   let solution = nums[0];
+//   for (let i = 1; i < arr.length; i++) {
+//     nums[i] = Math.max(nums[i], nums[i] + nums[i - 1]);
+//     solution = Math.max(solution, nums[i]);
+//   }
+//   return solution;
+// }
+
+// console.log(maxSubArray(arr));
